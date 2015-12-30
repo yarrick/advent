6625c76afdc1807b72e9228007b3a5bf56d68c41
Erik Ekman <erik@kryo.se>
2015-12-30 10:48:54 +0100

    Day 11

---
+++ 11.hs
@@ -0,0 +1,35 @@
+
+straight3 :: String -> Bool
+straight3 [] = False
+straight3 (a:[]) = False
+straight3 (a:b:[]) = False
+straight3 (a:b:c:dd)
+	| b == succ a && c == succ b = True
+	| otherwise = straight3 (b:c:dd)
+
+noiol :: String -> Bool
+noiol str = not (elem 'i' str || elem 'o' str || elem 'l' str)
+
+pairs :: String -> Int
+pairs [] = 0
+pairs (a:[]) = 0
+pairs (a:b:cc)
+	| a == b = 1 + pairs cc
+	| otherwise = pairs (b:cc)
+
+goodpassword :: String -> Bool
+goodpassword str = straight3 str && noiol str && pairs str >= 2
+
+step :: String -> String
+step [] = []
+step (a:bb)
+	| a == 'z' = 'a' : step bb
+	| otherwise = succ a : bb
+
+next :: String -> String
+next str = reverse $ step $ reverse str
+
+run :: String -> String
+run str
+	| goodpassword str = str
+	| otherwise = run $ next str
