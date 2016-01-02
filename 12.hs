ce3fccd5703de08f8be1a04baad49edc15658748
Erik Ekman <erik@kryo.se>
2016-01-02 13:24:02 +0100

    Day 12 complete

--- 12.hs
+++ 12.hs
@@ -1,27 +1,77 @@
-import Data.List
-import Data.Char
+import Data.Char (isDigit, isLetter)
 
-samenum :: Char -> Char -> Bool
-samenum a b
-	| b == '-' = False
-	| isnum a == isnum b = True
-	| otherwise = False
-	where isnum c = isDigit c || c == '-'
+data Tok = ListStart | ListEnd | ObjStart | ObjEnd | Comma | Colon | TString String | TInt Int
+	deriving Show
 
-allnum :: String -> Bool
-allnum [] = True
-allnum ('-':aa) = allnum aa
-allnum (a:bb) = isDigit a && allnum bb
+isNotNum :: Char -> Bool
+isNotNum c = not $ isDigit c || c == '-'
 
-goodnum :: String -> Bool
-goodnum "-" = False
-goodnum _ = True
+isNotStr :: Char -> Bool
+isNotStr c = not $ isLetter c || c == '\''
 
-numbers :: String -> [String]
-numbers str = filter goodnum $ filter allnum $ groupBy samenum str
+tokenize :: String -> [Tok]
+tokenize [] = []
+tokenize ('[':str) = ListStart : tokenize str
+tokenize (']':str) = ListEnd : tokenize str
+tokenize ('{':str) = ObjStart : tokenize str
+tokenize ('}':str) = ObjEnd : tokenize str
+tokenize (',':str) = Comma : tokenize str
+tokenize (':':str) = Colon : tokenize str
+tokenize str@('\'':_) = (TString word) : tokenize end
+	where (word, end) = break isNotStr str
+tokenize str = (TInt $ read num) : tokenize end
+	where (num, end) = break isNotNum str
+
+data Js = JsList [Js] | JsObj [Js] | JsInt Int | JsString String deriving Show
+
+-- these can be simplified..
+parseList :: [Tok] -> [Js] -> (Js, [Tok])
+parseList (ListEnd:tokens) js = (JsList js, tokens)
+parseList (TString str:tokens) js = parseList tokens (js ++ [JsString str])
+parseList (TInt num:tokens) js = parseList tokens (js ++ [JsInt num])
+parseList (Comma:tokens) js = parseList tokens js
+parseList (ListStart:listtok) js = parseList tokens (js ++ [sublist])
+	where (sublist, tokens) = parseList listtok []
+parseList (ObjStart:objtok) js = parseList tokens (js ++ [obj])
+	where (obj, tokens) = parseObj objtok []
+
+parseObj :: [Tok] -> [Js] -> (Js, [Tok])
+parseObj (ObjEnd:tokens) js = (JsObj js, tokens)
+parseObj (TString str:tokens) js = parseObj tokens (js ++ [JsString str])
+parseObj (TInt num:tokens) js = parseObj tokens (js ++ [JsInt num])
+parseObj (Comma:tokens) js = parseObj tokens js
+parseObj (Colon:tokens) js = parseObj tokens js
+parseObj (ListStart:listtok) js = parseObj tokens (js ++ [sublist])
+	where (sublist, tokens) = parseList listtok []
+parseObj (ObjStart:objtok) js = parseObj tokens (js ++ [obj])
+	where (obj, tokens) = parseObj objtok []
+
+parse :: [Tok] -> Js
+parse (ListStart:listtok) = list
+	where (list, tokens) = parseList listtok []
+parse (ObjStart:objtok) = obj
+	where (obj, tokens) = parseObj objtok []
+
+sumJs :: Int -> Js -> Int
+sumJs x (JsInt y) = x + y
+sumJs x (JsString _) = x
+sumJs x (JsList l) = x + foldl sumJs 0 l
+sumJs x (JsObj o) = x + foldl sumJs 0 o
 
 -- convert " to ' in inputfile to not mess up string argument
-solve :: String -> Int
-solve str = sum $ map read $ numbers str
+run str = sumJs 0 $ parse $ tokenize str
 
--- part 1 only for now..
+--part 2
+isred :: Js -> Bool
+isred (JsString "'red'") = True
+isred _ = False
+
+nored :: Js -> Js
+nored val@(JsInt i) = val
+nored val@(JsString s) = val
+nored (JsList list) = JsList (map nored list)
+nored (JsObj objs)
+	| any isred objs = JsObj []
+	| otherwise = JsObj (map nored objs)
+
+run2 str = sumJs 0 $ nored $ parse $ tokenize str
