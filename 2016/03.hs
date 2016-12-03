92dd64e5436df167dc49c48b720b8630bc90bc0e
Erik Ekman <erik@kryo.se>
2016-12-03 11:00:51 +0100

    Cleanup day 3

--- 2016/03.hs
+++ 2016/03.hs
@@ -1,14 +1,31 @@
 import Data.List
 
-check :: String -> Bool
-check str = (last all) < (sum $ take 2 all)
-  where all = sort $ parse str
+check :: [Int] -> Bool
+check nums = (last all) < (sum $ take 2 all)
+  where all = sort nums
 
 parse :: String -> [Int]
 parse str = map read $ words str
 
+part1 :: [String] -> String
+part1 rows = show $ length $ filter (==True) $ map check $ map parse rows
+
+-- part 2
+check3 :: [String] -> Int
+check3 [] = 0
+check3 (a:b:c:dd) = (length $ filter (==True) [left,mid,right]) + check3 dd
+  where pa = parse a
+        pb = parse b
+        pc = parse c
+        left  = check [pa!!0, pb!!0, pc!!0]
+        mid   = check [pa!!1, pb!!1, pc!!1]
+        right = check [pa!!2, pb!!2, pc!!2]
+
+part2 :: [String] -> String
+part2 rows = show $ check3 rows
+
 process :: [String] -> [String]
-process rows = [show $ length $ filter (==True) $ map check rows]
+process rows = [part1 rows, part2 rows]
 
 -- long file, lets do IO
 main :: IO ()
