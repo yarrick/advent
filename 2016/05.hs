0b6310b1751b4c0cffb1eec7ccdfc54b01ea83c5
Erik Ekman <erik@kryo.se>
2016-12-05 21:25:08 +0100

    Day 5

---
+++ 2016/05.hs
@@ -0,0 +1,39 @@
+import Crypto.Hash.MD5
+import qualified Data.ByteString.Char8 as B
+import qualified Data.Text as T
+import Data.HexString as H
+import Data.List
+
+md5sum :: String -> String
+md5sum str = T.unpack $ H.toText $ H.fromBytes $ hash $ B.pack str
+
+bruteforce :: ([String],Int,Int) -> String -> ([String],Int,Int)
+bruteforce (code,count,goal) door
+ | (length code) == goal = (code,count,goal)
+ | take 5 try == "00000" = bruteforce (code ++ [try],count+1,goal) door
+ | otherwise = bruteforce (code,count+1,goal) door
+  where try = md5sum $ door ++ show count
+
+part1 :: String -> String
+part1 door = getCode $ bruteforce ([],0,8) door
+  where getCode (a,b,c) = map (!!5) a
+
+--part 2
+conv :: [(Char,Char)] -> [(Int,Char)]
+conv [] = []
+conv ((a,b):cc)
+ | a >= '0' && a <= '7' = (read [a],b) : conv cc
+ | otherwise = conv cc
+
+cmp :: (Int,Char) -> (Int,Char) -> Ordering
+cmp (pa,ca) (pb,cb) = compare pa pb
+
+merge :: (String,Int) -> [(Int,Char)] -> (String,Int)
+merge a [] = a
+merge (code,prev) ((pos,c):cc)
+ | pos == prev + 1 = merge (code ++ [c],pos) cc
+ | otherwise = merge (code,prev) cc
+
+part2 :: String -> String
+part2 door = fst $ merge ("",-1) $ sortBy cmp $ conv $ getCode $ bruteforce ([],0,34) door
+  where getCode (a,b,c) = map (\x -> (x!!5,x!!6)) a
