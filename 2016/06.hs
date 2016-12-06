94d6b18e712e26399c64eb74dbafaa71a4e3e035
Erik Ekman <erik@kryo.se>
2016-12-06 08:55:48 +0100

    Day 6 cleaner

--- 2016/06.hs
+++ 2016/06.hs
@@ -10,18 +10,14 @@ addCount ((al,alcount):bb) c
  | al == c = (al,alcount+1) : bb
  | otherwise = (al,alcount) : addCount bb c
 
-getTop :: [(Char,Int)] -> String
-getTop count = [fst $ head count]
-
-decode :: [String] -> ((Char,Int) -> (Char,Int) -> Ordering) -> String
-decode rows cmp = foldl1 (++) $ map signal $ transpose rows
-  where signal x = getTop $ sortBy cmp $ countAlpha [] x
+decode :: ((Char,Int) -> (Char,Int) -> Ordering) -> String -> Char
+decode cmp x = fst $ head $ sortBy cmp $ countAlpha [] x
 
 process :: [String] -> [String]
-process rows = [decode rows most, decode rows least]
+process rows = [map (decode most) rows, map (decode least) rows]
   where most (ca,va) (cb,vb) = compare vb va
         least (ca,va) (cb,vb) = compare va vb
 
 -- long file, lets do IO
 main :: IO ()
-main = interact (unlines . process . lines)
+main = interact (unlines . process . transpose . lines)
