43ceac41be07e04f546330e66c7aec871401f575
Erik Ekman <erik@kryo.se>
2019-12-08 15:00:27 +0100

    day 9 part 2

--- 2016/09.hs
+++ 2016/09.hs
@@ -17,4 +17,20 @@ process rows = map (show . expand) rows
 
 -- long file, lets do IO
 main :: IO ()
-main = interact (unlines . process . lines)
+main = interact (unlines . process2 . lines)
+
+-- part 2
+
+expand2 :: String -> Int
+expand2 [] = 0
+expand2 ('(':bb) = expand2 expanded + expand2 nstr
+  where (exp,tt) = break (')'==) bb
+        (count,rep) = parse exp
+        repstr = take count $ tail tt
+        nstr = drop count $ tail tt
+        expanded = take (count*rep) (cycle repstr)
+expand2 (_:bb) = 1 + expand2 bb
+
+process2 :: [String] -> [String]
+process2 rows = map (show . expand2) rows
+
