2b174c71d8c81b20ef8a7d0fd900cbd5b7176160
Erik Ekman <erik@kryo.se>
2019-12-12 20:29:03 +0100

    Day 16, 2016

---
+++ 2016/16.hs
@@ -0,0 +1,26 @@
+
+dragon :: String -> Int -> String
+dragon a minlen
+  | length next >= minlen = take minlen next
+  | otherwise = dragon next minlen
+  where b = invert $ reverse a
+        next = a ++ "0" ++ b
+
+invert :: String -> String
+invert [] = []
+invert ('1':is) = '0' : invert is
+invert ('0':is) = '1' : invert is
+
+chksum :: String -> String
+chksum [] = []
+chksum (a:b:cs)
+  | a == b = '1' : chksum cs
+  | otherwise = '0' : chksum cs
+
+csum :: String -> String
+csum a
+  | odd $ length first = first
+  | otherwise = csum first
+  where first = chksum a
+
+run str len = csum $ dragon str len
