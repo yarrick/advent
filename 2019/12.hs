815da72bfea3c3e1a9ee1eb687559c7ac6ae5a7e
Erik Ekman <erik@kryo.se>
2019-12-12 19:07:03 +0100

    Day 12 complete

--- 2019/12.hs
+++ 2019/12.hs
@@ -26,18 +26,32 @@ applygrav (m@(a,aa):as) diffs@((b,bb):bs)
   | a < b = m : applygrav as diffs
   | a == b = applygrav ((a, map (\((p,v),d) -> (p,v+d)) $ zip aa bb):as) bs
 
-grav ms = applygrav ms deltav
+tick :: [Moon] -> a ->  [Moon]
+tick ms _ = map move $ applygrav ms deltav
   where
     biggermoon (a,_) (b,_) = b > a
     deltav = sort $ concat $ map gravadd $ [ (a,b) | a <- ms, b <- filter (biggermoon a) ms ]
-
-tick :: [Moon] -> a ->  [Moon]
-tick ms _ = map move $ grav ms
-  where move (idx, pv) = (idx, [ (p + v, v) | (p,v) <- pv ])
+    move (idx, pv) = (idx, [ (p + v, v) | (p,v) <- pv ])
 
 energy :: Moon -> Int
 energy (_,posvel) = (sum $ map abs ps) * (sum $ map abs vs)
   where (ps,vs) = unzip posvel
 
+run :: [String] -> Int -> Int
 run str iter = sum $ map energy $ foldl tick moons $ take iter [0..]
+  where moons = map moon $ zip [0..] $ map parse str
+
+-- part 2
+
+axes :: [Moon] -> [[Moon]]
+axes m = map (extract 0) m : map (extract 1) m : map (extract 2) m : []
+  where extract pos (idx,ax) = (idx, [ax !! pos])
+
+findloop :: [Moon] -> [Moon] -> Int -> Int
+findloop goal cur index
+  | next == goal = index
+  | otherwise = findloop goal next (index+1)
+  where next = tick cur 0
+
+run2 str = foldl1 lcm $ map (\m -> findloop m m 1) $ axes moons
   where moons = map moon $ zip [0..] $ map parse str
