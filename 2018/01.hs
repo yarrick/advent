bb2295bd182534b8df2ad2852f5e664e68c9c35b
Erik Ekman <erik@kryo.se>
2019-12-31 17:25:55 +0100

    Day 1, 2018

---
+++ 2018/01.hs
@@ -0,0 +1,20 @@
+parse :: String -> (Int -> Int)
+parse (c:num)
+  | c == '+' = (+n)
+  | c == '-' = (+(-n))
+  where n = read num
+
+firstdup :: [Int] -> [Int] -> Int
+firstdup seen (x:xs)
+  | elem x seen = x
+  | otherwise = firstdup (x:seen) xs
+
+process :: [String] -> [String]
+process rows = [show $ foldl apply 0 ops,
+                show $ firstdup [] $ scanl apply 0 $ cycle ops ]
+  where apply n fn = fn n
+        ops = map parse rows
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . lines)
