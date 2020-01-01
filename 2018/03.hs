fa0e6f5c0c8c19a7589fdf63329c7c8b712fbc3d
Erik Ekman <erik@kryo.se>
2020-01-01 12:00:23 +0100

    Day 3, 2018

---
+++ 2018/03.hs
@@ -0,0 +1,31 @@
+import Data.List
+
+-- id, topleft, size
+type Claim = (Int, (Int,Int), (Int,Int))
+
+parse :: String -> Claim
+parse str = (read $ tail cid,(read col,read $ take (length row - 2) $ tail row),(read width,read $ tail xheight))
+  where [cid,_,start,size] = words str
+        (width,xheight) = break ('x'==) size
+        (col,row) = break (','==) start
+
+squares :: Claim -> [((Int,Int),Int)]
+squares (cid,(col,row),(width,height)) =
+  [ ((i,j),cid) | i <- [row..(row+height-1)], j <- [col..(col+width-1)] ]
+
+dups :: (Int,[Int]) -> [((Int,Int),Int)] -> [Int]
+dups (n,ids) [a] = [n,head ids]
+dups (n,ids) cs@(((ia,ja),_):s@((ib,jb),_):ss)
+  | ia /= ib || ja /= jb = dups (n,ids) (s:ss)
+  | otherwise = dups (n+1,nids) (dropWhile (samepos (ia,ja)) cs)
+  where samepos (c,r) ((i,j),_) = c == i && r == j
+        nids = foldl (\cids dup -> delete dup cids) ids $ map snd $ takeWhile (samepos (ia,ja)) cs
+
+process :: [String] -> [String]
+process rows = map show $ dups (0,claimids) $ sort $ concatMap squares claims
+  where claims = map parse rows
+        claimids = map (\(cid,_,_) -> cid) claims
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . lines)
