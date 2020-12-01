b303181ae88afd1719c2cdc1319a5016a428ed11
Erik Ekman <erik@kryo.se>
2020-12-01 07:46:31 +0100

    minor cleanup

--- 2020/01.hs
+++ 2020/01.hs
@@ -2,27 +2,27 @@ import Data.List
 import Data.Maybe
 
 part1 :: ([Int],[Int]) -> Int
-part1 (lo,hi) = head $ summer 2020 lo hi
+part1 (lo,hi) = fromJust $ summer 2020 lo hi
 
-summer :: Int -> [Int] -> [Int] -> [Int]
-summer _ [] _ = []
-summer _ _ [] = []
+summer :: Int -> [Int] -> [Int] -> Maybe Int
+summer _ [] _ = Nothing
+summer _ _ [] = Nothing
 summer goal (l:ls) (h:hs)
-    | l + h == goal = [l * h]
+    | l + h == goal = Just (l * h)
     | l + h < goal = summer goal ls (h:hs)
     | l + h > goal = summer goal (l:ls) hs
 
 part2 :: Int -> ([Int],[Int]) -> Int
 part2 pos (lo,hi)
-    | triplet == [] = part2 (pos+1) (lo,hi)
-    | otherwise = base * (head triplet)
+    | isJust triplet = base * fromJust triplet
+    | otherwise = part2 (pos+1) (lo,hi)
     where base = lo !! pos
-          triplet = summer (2020 - base) (take pos lo ++ drop (pos+1) lo) hi
+          triplet = summer (2020 - base) lo hi
 
 process :: [String] -> [String]
 process rows = map show [part1 groups, part2 0 groups]
-    where nums = map read rows
-          groups = (sort nums, reverse $ sort nums)
+    where nums = sort $ map read rows
+          groups = (nums, reverse nums)
 
 -- long file, lets do IO
 main :: IO ()
