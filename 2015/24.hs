5165b1a1d65e385732d73e764b4c80d2a2955849
Erik Ekman <erik@kryo.se>
2020-12-06 11:35:23 +0100

    Day 24, 2015
    Finally completed 2015!
    
    Brute forcing generation of all sets. By starting with the highest
    number and reducing duplicates when generating by only putting each
    number into one empty group I can get away with stopping after
    500k checks without any improvement and still get the right answer.
    
    Runs in about a minute.

---
+++ 2015/24.hs
@@ -0,0 +1,43 @@
+import Data.List
+
+addone :: Int -> Int -> [[Int]] -> Int -> [[[Int]]]
+addone limit val groups pos
+    | pos == length groups = []
+    | length (groups !! pos) == 0 = [grown]
+    | fits = grown : addone limit val groups (pos+1)
+    | otherwise = addone limit val groups (pos+1)
+    where fits = sum (groups !! pos) + val <= limit
+          grown = (take pos groups) ++ [(groups !! pos) ++ [val]] ++ (drop (pos+1) groups)
+
+combos :: Int -> [[Int]] -> [Int] -> [[[Int]]]
+combos _ a [] = [a]
+combos limit prev (i:is)
+    | length added > 0 = concat $ map (\a -> combos limit a is) added
+    | otherwise = []
+    where added = addone limit i prev 0
+
+score :: [[Int]] -> (Int,Int)
+score b = (length b1, product b1)
+    where b1 = head $ sortBy (\x y -> compare (length x) (length y)) b
+
+better :: (Int,Int,Int) -> [(Int,Int)] -> [(Int,Int)]
+better _ [] = []
+better (bestsize,bestent,skips) (x@(s,e):xs)
+    | s < bestsize = x : better (s,e,0) xs
+    | s == bestsize && e < bestent = x : better (s,e,0) xs
+    | skips > 500000 = [] -- random give up point
+    | otherwise = better (bestsize,bestent,skips+1) xs
+
+run :: [Int] -> Int -> [(Int,Int)]
+run nums bags = better (length buckets,1,0) $ map score sets
+  where buckets = reverse $ sort nums
+        groupsize = div (sum nums) bags
+        sets = combos groupsize (replicate bags []) buckets
+
+process :: [String] -> [String]
+process rows = map (show . snd . last) [run nums 3, run nums 4]
+    where nums = map read rows
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . lines)
