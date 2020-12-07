88969bc81cc9e27a4e367e28bb29ee6168fb280d
Erik Ekman <erik@kryo.se>
2020-12-08 00:29:37 +0100

    Day 9, 2018 complete
    Switch from list to Data.Sequence
    
    Part one speedup is from 136.4 to 2.4 seconds!
    Part two done in 236 seconds.

--- 2018/09.hs
+++ 2018/09.hs
@@ -1,23 +1,22 @@
-import Control.DeepSeq
+import qualified Data.Sequence as S
 
-type Circle = ([Int],Int)
+type Circle = (S.Seq Int,Int)
 
 place :: Circle -> Int -> Circle
 place (circle,curr) new
-  | newpos == 0 = (circle ++ [new], length circle)
-  | otherwise = deepseq newcircle (newcircle,newpos)
-  where newpos = mod (curr + 2) (length circle)
-        newcircle = (take newpos circle) ++ [new] ++ (drop newpos circle)
+  | newpos == 0 = (circle S.|> new, S.length circle)
+  | otherwise = (newcircle,newpos)
+  where newpos = mod (curr + 2) (S.length circle)
+        newcircle = S.insertAt newpos new circle
 
 steal :: Circle -> (Int,Circle)
-steal (circle,curr) = (circle!!stealpos,
-  (take stealpos circle ++ drop (stealpos+1) circle, stealpos))
-  where stealpos = mod (curr - 7) (length circle)
+steal (circle,curr) = (S.index circle stealpos, (S.deleteAt stealpos circle,stealpos))
+  where stealpos = mod (curr - 7) (S.length circle)
 
 play :: (Circle,[Int]) -> Int -> (Circle,[Int])
 play (circle, (score:ss)) marble
-  | mod marble 23 == 0 = deepseq newscore (ncircle, newscore)
-  | otherwise = deepseq movescore (place circle marble,movescore)
+  | mod marble 23 == 0 = (ncircle, newscore)
+  | otherwise = (place circle marble,movescore)
   where (stolen, ncircle) = steal circle
         marblescore = marble+stolen
         newscore = ss++[score+marblescore]
@@ -30,4 +29,4 @@ game lastscore state marble
   where nstate = play state marble
 
 run players lastscore = maximum playerscore
-  where (circle, playerscore) = game lastscore (([0],0),take players $ repeat 0) 1
+  where (circle, playerscore) = game lastscore ((S.singleton 0,0),replicate players 0) 1
