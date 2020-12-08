681de4f062c3aede722ddd00b7806924e1d85d2d
Erik Ekman <erik@kryo.se>
2020-12-08 14:37:08 +0100

    Day 5, 2017

---
+++ 2017/05.hs
@@ -0,0 +1,21 @@
+import Data.List
+
+steps :: [Int] -> Int -> Int -> (Int -> Int) -> Int
+steps jumps count pos fn
+    | pos >= length jumps = count
+    | otherwise = steps nextjumps (count+1) nextpos fn
+    where val = jumps !! pos
+          nextpos = pos + val
+          nextjumps = take pos jumps ++ [fn val] ++ drop (pos+1) jumps
+
+process :: [String] -> [String]
+process rows = map show [steps nums 0 0 succ, steps nums 0 0 nextjump]
+    where nums = map read rows
+          nextjump x
+            | x >= 3 = x-1
+            | otherwise = x+1
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . lines)
+
