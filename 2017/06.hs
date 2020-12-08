36129f1a67842090deac96bf209f9c30d38b7da5
Erik Ekman <erik@kryo.se>
2020-12-08 16:18:19 +0100

    Day 6, 2017

---
+++ 2017/06.hs
@@ -0,0 +1,38 @@
+import Data.List
+import Debug.Trace as T
+
+pickPos :: [Int] -> Int
+pickPos state = snd $ head $ sortBy rank placed
+    where placed = zip state [0..]
+          rank (sa,pa) (sb,pb)
+            | sa == sb = compare pa pb
+            | otherwise = compare sb sa -- descending sort
+
+add :: [Int] -> [Int] -> [Int]
+add st ad
+    | len < length ad = add (inc st (take len ad)) (drop len ad)
+    | otherwise = inc st ad
+    where len = length st
+          inc [] [] = []
+          inc a [] = a
+          inc (a:as) (b:bs) = (a+b) : add as bs
+
+run :: [Int] -> [[Int]] -> ([Int],Int)
+run state hist
+    | elem state hist = (state, length hist)
+    | otherwise = run (add base adds) (state:hist)
+    where pos = pickPos state
+          val = state !! pos
+          adds = (replicate (pos+1) 0) ++ (replicate val 1)
+          base = take pos state ++ [0] ++ drop (pos+1) state
+
+process :: [String] -> [String]
+process rows = map show [cycles, cycles2]
+    where nums = map read $ concatMap words rows
+          (repeated, cycles) = run nums []
+          (repeated2, cycles2) = run repeated []
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . lines)
+
