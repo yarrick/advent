89f45ebf8b438cbf0dfd07b72e3111eece3a799d
Erik Ekman <erik@kryo.se>
2020-12-09 15:02:06 +0100

    Day 13, 2017
    Runs in under 8 minutes, good enough.

---
+++ 2017/13.hs
@@ -0,0 +1,42 @@
+import Data.Char
+import Data.List
+
+type Level = (Int,[Int])
+
+parse :: [String] -> Level
+parse (lr:d:ss) = (read lvl, lvlpos $ read d)
+    where lvl = takeWhile isDigit lr
+
+lvlpos d = d : replicate (2*(d-1) - 1) 0
+
+levels :: Int -> [Level] -> [Level]
+levels _ [] = []
+levels d (l@(lr,_):lvls)
+    | d == lr = l : levels (succ d) lvls
+    | otherwise = (d, [0]) : levels (succ d) (l:lvls)
+
+run :: Int -> [Level] -> Int
+run _ [] = 0
+run delay ((d,lpos):lvls) = d * ((cycle lpos) !! delay) + run (succ delay) lvls
+
+pass :: Level -> Int -> Bool
+pass (lr,lvl) delay = (cycle lvl) !! (delay+lr) == 0
+
+grow :: (Int,[Int]) -> Int -> [Int]
+grow (n,passes) size = sort [ (a+n*b) | a <- passes, b <- [0..factor-1]]
+    where factor = quot size n
+
+gap :: (Int,[Int]) -> Level -> (Int,[Int])
+gap (len,passes) lvl = (size, filter (pass lvl) grown)
+    where size = lcm len (length $ snd lvl)
+          grown = grow (len,passes) size
+
+process :: [String] -> [String]
+process rows = map show [run 0 lvls, head gaps]
+    where lvls = levels 0 $ map (parse . words) rows
+          (_,gaps) = foldl gap (1,[0]) lvls
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . lines)
+
