fc5a44c8924a3fa78d8058687d23126d1e46943e
Erik Ekman <erik@kryo.se>
2020-12-09 09:43:08 +0100

    Day 25, 2018

---
+++ 2018/25.hs
@@ -0,0 +1,23 @@
+import Data.List
+
+parse :: String -> [Int]
+parse [] = []
+parse str = read next : parse (drop (length next+1) str)
+    where next = takeWhile (\c -> c /= ',') str
+
+nearby :: [Int] -> [Int] -> Bool
+nearby a b = dist <= 3
+    where dist = sum $ map abs $ zipWith (-) a b
+
+build :: [[[Int]]] -> [Int] -> [[[Int]]]
+build cst star
+    | length matches > 0 = (star : concat matches) : other
+    | otherwise = [star] : cst
+    where (matches,other) = partition (any (nearby star)) cst
+
+process :: [[Int]]  -> [String]
+process stars = [show $ length $ foldl build [] stars]
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . (map parse) . lines)
