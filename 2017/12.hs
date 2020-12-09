fa00ca2d3b302b2bf2cc56fde789cec5e023d9cd
Erik Ekman <erik@kryo.se>
2020-12-09 12:22:04 +0100

    Day 12, 2017
    Similar to day 25, 2018

---
+++ 2017/12.hs
@@ -0,0 +1,23 @@
+import Data.List
+import Data.Char
+
+parse :: [String] -> [Int]
+parse (s:arrow:nums) = nub $ (read s) : map get nums
+    where get t = read $ takeWhile isDigit t
+
+connected :: [Int] -> [Int] -> Bool
+connected g h = any (\x -> elem x g) h
+
+build :: [[Int]] -> [Int] -> [[Int]]
+build groups g
+    | length matches > 0 = (nub $ concat (g:matches)) : other
+    | otherwise = g : groups
+    where (matches,other) = partition (connected g) groups
+
+process :: [[Int]]  -> [String]
+process groups = map show [length $ head $ filter (elem 0) nets, length $ nets]
+    where nets = foldl build [] groups
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . (map (parse . words)) . lines)
