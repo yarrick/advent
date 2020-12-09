79f534fb424df8a20c0038798f4b14597f4be7d9
Erik Ekman <erik@kryo.se>
2020-12-09 11:56:36 +0100

    Minor knot cleanup

--- 2017/10.hs
+++ 2017/10.hs
@@ -1,9 +1,7 @@
 import Knot
-import Data.Bits
-import Text.Printf
 
 part1 :: [Int] -> Int
-part1 twists = product $ take 2 $ run [0..255] twists
+part1 twists = product $ take 2 $ run twists
 
 part2 :: String -> String
 part2 = knot
--- 2017/Knot.hs
+++ 2017/Knot.hs
@@ -1,4 +1,3 @@
-
 module Knot where
 
 import Data.Char
@@ -15,17 +14,16 @@ step :: ([Int],Int) -> (Int,Int) -> ([Int],Int)
 step (ts,moves) (pos,skip) = (move (twist ts pos) offset, moves+offset)
     where offset = (pos+skip)
 
-run :: [Int] -> [Int] -> [Int]
-run list twists = move knotted extramoves -- restore starting pos
-    where (knotted,moves) = foldl step (list,0) $ zip twists [0..]
-          extramoves = length list - mod moves (length list)
+run :: [Int] -> [Int]
+run twists = move knotted extramoves -- restore starting pos
+    where (knotted,moves) = foldl step ([0..255],0) $ zip twists [0..]
+          extramoves = length knotted - mod moves (length knotted)
 
 hexed :: [Int] -> String
 hexed [] = []
 hexed nums = printf "%02x" val ++ hexed (drop 16 nums)
     where val = foldl1 xor (take 16 nums)
 
-knot str = hexed $ run [0..255] twists
+knot str = hexed $ run $ take (64 * length input) $ cycle input
     where input = map ord str ++ [17, 31, 73, 47, 23]
-          twists = take (64 * length input) $ cycle input
 
