225bd83ce7b5666496e5044972bbaeb5502bf6f6
Erik Ekman <erik@kryo.se>
2020-12-09 08:35:09 +0100

    Day 9, 2020

---
+++ 2020/09.hs
@@ -0,0 +1,25 @@
+misses :: [Int] -> [Int] -> Int
+misses pre (x:xs)
+    | elem x sums = misses newpre xs
+    | otherwise = x
+    where prerange = pred $ length pre
+          sums = [(pre !! a) + (pre !! b) | a <- [0..prerange], b <- [0..prerange], a /= b ]
+          newpre = x : (take prerange pre)
+
+sumrange :: Int -> Int -> [Int] -> [Int] -> [Int]
+sumrange goal score guess nums
+    | score == goal = guess
+    | score > goal = sumrange goal (score - head guess) (tail guess) nums
+    | score < goal = sumrange goal (score + cand) (guess ++ [cand]) (tail nums)
+    where cand = head nums
+
+process :: [String] -> [String]
+process rows = map show $ [firstmiss, minimum rangescore + maximum rangescore]
+    where nums = map read rows
+          getmiss n prelen = misses (reverse $ take prelen n) (drop prelen n)
+          firstmiss = getmiss nums 25
+          rangescore = sumrange firstmiss 0 [] nums
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . lines)
