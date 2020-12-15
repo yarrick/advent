d638853646255a06ae44448b05a735d984c5cfda
Erik Ekman <erik@kryo.se>
2020-12-15 14:12:40 +0100

    Day 14, 2018 complete
    Converted from Vector to Sequence also

--- 2018/14.hs
+++ 2018/14.hs
@@ -1,21 +1,39 @@
-import qualified Data.Vector as V
+import qualified Data.Sequence as S
+import Data.Foldable
 
-type Recipes = (V.Vector Int,Int,Int)
+type Recipes = (S.Seq Int,Int,Int)
 
-results :: Int -> V.Vector Int
-results n
-  | n < 10 = V.singleton n
-  | otherwise = V.cons 1 (V.singleton (mod n 10))
+startRec = (S.fromList [3,7], 0, 1)
+
+append :: S.Seq Int -> Int -> S.Seq Int
+append seq n
+  | n < 10 = seq S.|> n
+  | otherwise = (seq S.|> 1) S.|> (mod n 10)
 
 step :: Recipes -> Recipes
-step (scores,cura,curb) = (scores V.++ (results newres), newpos cura, newpos curb)
-  where newpos n = mod (n + 1 + (scores V.! n)) (length scores + length (results newres))
-        newres = (scores V.! cura) + (scores V.! curb)
+step (scores,cura,curb) = (newscores, newpos cura, newpos curb)
+  where newres = (S.index scores cura) + (S.index scores curb)
+        newscores = append scores newres
+        newpos n = mod (n + 1 + (S.index scores n)) (length newscores)
 
-flow :: Int -> Int -> Recipes -> Recipes
-flow n loglen rc@(rec,_,_)
+flow :: Int -> Recipes -> Recipes
+flow n rc@(rec,_,_)
   | length rec >= n + 10 = rc
-  | otherwise = flow n loglen $ step rc
+  | otherwise = flow n $ step rc
 
-run n = concatMap show $ take 10 $ drop n $ V.toList vec
-  where (vec,a,b) = flow n 1000 (V.cons 3 (V.singleton 7),0,1)
+run :: Int -> String
+run n = concatMap show $ take 10 $ drop n $ toList seq
+  where (seq,a,b) = flow n startRec
+
+flow2 :: [Int] -> Recipes -> S.Seq Int
+flow2 wanted rc@(rec,_,_)
+  | wanted == end = rec
+  | wanted == take (length wanted) nend = S.deleteAt (S.length rec-1) rec
+  | otherwise = flow2 wanted $ step rc
+  where end = reverse $ toList $ S.take (length wanted) $ S.reverse rec
+        nend = reverse $ toList $ S.take (length wanted + 1) $ S.reverse rec
+
+run2 :: Int -> Int
+run2 n = S.length seq - length wanted
+  where wanted = [read [a] | a <- show n ]
+        seq = flow2 wanted startRec
