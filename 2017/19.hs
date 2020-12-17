4e04f07e13dbf1e73cff04ec8aac23c53b967b3c
Erik Ekman <erik@kryo.se>
2020-12-17 11:05:42 +0100

    Day 19, 2017

---
+++ 2017/19.hs
@@ -0,0 +1,42 @@
+import Data.Char
+import Data.Matrix
+
+type Direction = (Int,Int)
+up = (-1,0)
+left = (0,-1)
+right = (0,1)
+down = (1,0)
+
+turns :: Direction -> [Direction]
+turns dir
+    | elem dir [up,down] = [left,right]
+    | otherwise = [up,down]
+
+step :: (Int,Int) -> Direction -> (Int,Int)
+step (r,c) (dr,dc) = (r+dr, c+dc)
+
+peek :: Matrix Char -> (Int,Int) -> Direction -> Char
+peek m pos dir
+    | r < 1 || c < 1 || r > nrows m || c > ncols m = ' '
+    | otherwise = m ! (r,c)
+    where (r,c) = step pos dir
+
+walk :: Matrix Char -> (Int,Int) -> Direction -> String
+walk m pos dir
+    | not (elem nc " +") = nc : walk m (step pos dir) dir
+    | length options == 0 = []
+    | otherwise = nc : walk m (step pos newdir) newdir
+    where nc = m ! pos
+          options = filter (\(_,c) -> c /= ' ') [ (d, peek m pos d) | d <- turns dir ]
+          (newdir, nnc) = head options
+
+process :: Matrix Char -> [String]
+process m = [show $ filter isAlpha path, show $ length path]
+    where startCols = zip [1..] $ map (\c -> m ! (1,c)) [1..ncols m]
+          startPos = (1, fst $ head $ filter (\(_,c) -> c == '|') startCols)
+          path = (m!startPos) : (walk m (step startPos down) down)
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . fromLists . lines)
+
