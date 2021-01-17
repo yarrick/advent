57d23a12f1d2cfd4920515c25f84df347abb89b7
Erik Ekman <erik@kryo.se>
2021-01-17 12:08:11 +0100

    Day 17, 2017 complete
    Part 2 runs in 70s.

--- 2017/17.hs
+++ 2017/17.hs
@@ -1,4 +1,5 @@
 import qualified Data.Sequence as S
+import Data.Foldable
 
 type Ring = (S.Seq Int, Int)
 
@@ -9,4 +10,17 @@ step gap (r,p) n = (S.insertAt pos n r, pos)
 run :: Int -> Int
 run n = S.index end (pos+1)
     where r = (S.singleton 0, 0)
-          (end,pos) = foldl (step n) r [1..2017]
+          (end,pos) = foldl' (step n) r [1..2017]
+
+-- value after 0, total length, pos
+type FastRing = (Int, Int, Int)
+
+fstep :: Int -> FastRing -> Int -> FastRing
+fstep gap (zv,len,p) n
+    | npos == 1 = (n, succ len, npos)
+    | otherwise = (zv, succ len, npos)
+    where npos = 1 + mod (p+gap) len
+
+run2 :: Int -> Int
+run2 n = zv
+    where (zv,_,_) = foldl' (fstep n) (0,1,0) [1..50000000]
