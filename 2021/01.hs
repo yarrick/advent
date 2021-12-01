5841c0dd1a1f8ac540ed18e57e422742fa22d9ec
Erik Ekman <erik@kryo.se>
2021-12-01 22:28:46 +0100

    Shorter day 1

--- 2021/01.hs
+++ 2021/01.hs
@@ -1,20 +1,13 @@
-
-part1 :: Int -> [Int] -> Int
-part1 num (a:[]) = num
-part1 deeper (a:b:cc)
-    | b > a = part1 (succ deeper) (b:cc)
-    | otherwise = part1 deeper (b:cc)
-
-part2 :: Int -> [Int] -> Int
-part2 deeper nums
-    | length nums < 4 = deeper
-    | b > a = part2 (succ deeper) (tail nums)
-    | otherwise = part2 deeper (tail nums)
-    where a = sum $ take 3 nums
-          b = sum $ take 3 $ tail nums
+solve :: Int -> Int -> [Int] -> Int
+solve glen deeper nums
+    | length nums < succ glen = deeper
+    | b > a = solve glen (succ deeper) (tail nums)
+    | otherwise = solve glen deeper (tail nums)
+    where a = sum $ take glen nums
+          b = sum $ take glen $ tail nums
 
 process :: [String] -> [String]
-process rows = map show [part1 0 nums, part2 0 nums]
+process rows = map show [solve 1 0 nums, solve 3 0 nums]
     where nums = map read rows
 
 -- long file, lets do IO
