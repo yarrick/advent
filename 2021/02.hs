ddff6420033187149fc261551281fac72a0f2950
Erik Ekman <erik@kryo.se>
2021-12-02 10:41:34 +0100

    Day 2, 2021

---
+++ 2021/02.hs
@@ -0,0 +1,22 @@
+drive :: (Int, Int, Int) -> (String, Int) -> (Int, Int, Int)
+drive (h,v,_) ("up",len) = (h, v-len, 0)
+drive (h,v,_) ("down",len) = (h, v+len, 0)
+drive (h,v,_) ("forward",len) = (h+len, v, 0)
+
+drive2 :: (Int, Int, Int) -> (String, Int) -> (Int, Int, Int)
+drive2 (h,v,aim) ("up", len) = (h,v,aim+len)
+drive2 (h,v,aim) ("down", len) = (h,v,aim-len)
+drive2 (h,v,aim) ("forward",len) = (h+len,v-aim*len,aim)
+
+parse :: [String] -> (String, Int)
+parse (dir:len:[]) = (dir, read len)
+
+process :: [String] -> [String]
+process rows = map res [foldl drive (0,0,0) steps, foldl drive2 (0,0,0) steps]
+    where steps = map (parse.words) rows
+          res (a,b,_) = show $ a * b
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . lines)
+
