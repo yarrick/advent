99b7fc90dcca5a0652ed61ba2093fbf7d3178499
Erik Ekman <erik@kryo.se>
2021-12-03 12:17:08 +0100

    Day 3, 2021

---
+++ 2021/03.hs
@@ -0,0 +1,33 @@
+import Data.List
+
+closest :: Int -> (Int -> Int -> Bool) -> [[Int]] -> [Int]
+closest _ _ [a] = a
+closest n op cands = closest (succ n) op $ filter good cands
+    where goal = part1 cands op
+          good nums = goal !! n == nums !! n
+
+part2 :: [[Int]] -> (Int -> Int -> Bool) -> [Int]
+part2 bits op = closest 0 op bits
+
+part1 :: [[Int]] -> (Int -> Int -> Bool) -> [Int]
+part1 bits op = map (picker op) $ map (\r -> (blen r 0, blen r 1)) $ transpose bits
+    where blen l val = length $ fst $ partition (==val) l
+
+picker :: (Int -> Int -> Bool) -> (Int, Int) -> Int
+picker op (a,b)
+    | op a b = 0
+    | otherwise = 1
+
+parse :: String -> [Int]
+parse num = map (\x -> read [x]) num
+
+process :: [String] -> [String]
+process rows = map (show.solve) [part1, part2]
+    where bits = map parse rows
+          deci n = foldl (\a b -> 2*a+b) 0 n
+          solve p = product $ map (deci.p bits) [(>), (<=)]
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . lines)
+
