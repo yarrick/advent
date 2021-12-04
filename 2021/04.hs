b6312d3a4d0dbf58a51f493b765c72cd356478ed
Erik Ekman <erik@kryo.se>
2021-12-04 10:45:03 +0100

    Day 4, 2021

---
+++ 2021/04.hs
@@ -0,0 +1,36 @@
+import Data.Char
+import Data.List
+
+type Board = [[Int]]
+
+bingo :: [Board] -> [Int] -> Int -> [([Board], [Int])]
+bingo [] _ _ = []
+bingo bs guesses num
+    | winners == [] = bingo bs guesses (succ num)
+    | otherwise = (winners, take num guesses) : bingo losers guesses (succ num)
+    where winners = filter (winner $ take num guesses) bs
+          losers = filter (\b -> notElem b winners) bs
+
+score :: ([Board], [Int]) -> Int  -- only scores first board in list.
+score (bs, gs) = last gs * unmarked
+    where unmarked = sum $ filter (\n -> notElem n gs) (concat $ head bs)
+
+winner :: [Int] -> Board -> Bool
+winner guesses rows = any rowwin $ rows ++ transpose rows
+    where rowwin rr = all (\r -> elem r guesses) rr
+
+parse :: [String] -> ([Int], [Board])
+parse (g:bs) = (map read $ filter (/=",") $ groupBy isnum g, chunk bs)
+    where isnum a b = isDigit a == isDigit b
+          chunk [] = []
+          chunk rs = (map (map read.words) $ tail $ take 6 rs) : chunk (drop 6 rs)
+
+process :: [String] -> [String]
+process rows = map (show.score) [head result, last result]
+    where (guesses,boards) = parse rows
+          result = bingo boards guesses 1
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . lines)
+
