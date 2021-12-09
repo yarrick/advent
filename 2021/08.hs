e0dcc89e852cca9f48b11600284867598defad43
Erik Ekman <erik@kryo.se>
2021-12-09 19:07:08 +0100

    Simpler day 8

--- 2021/08.hs
+++ 2021/08.hs
@@ -4,27 +4,26 @@ easyDigit :: String -> Bool
 easyDigit s = elem (length s) [2, 4, 3, 7]
 
 deduct :: [String] -> [String]
-deduct leds = map sort [zero,one,two,three,four,five,six,seven,eight,nine]
+deduct leds = map (sort.head) [zero,one,two,three,four,five,six,seven,eight,nine]
     where getLen l ls = filter (\n -> l==length n) ls
-          diff dig chars = filter (\s -> notElem s (concat chars)) dig
-          uniq l = getLen 1 $ group $ sort l
-          one = head $ getLen 2 leds
-          seven = head $ getLen 3 leds
-          four = head $ getLen 4 leds
+          diff dig chars = filter (`notElem` (concat chars)) $ concat dig
+          uniq l = concat $ getLen 1 $ group $ sort l
+          one = getLen 2 leds
+          seven = getLen 3 leds
+          four = getLen 4 leds
           two_three_five = getLen 5 leds
           zero_six_nine = getLen 6 leds
-          eight = head $ getLen 7 leds
-          aseg = head $ diff seven [one]
-          beseg = concat $ uniq $ concat two_three_five
-          eseg = head $ diff beseg [four]
-          bseg = head $ diff beseg [[eseg]]
-          two = head $ filter (elem eseg) two_three_five
-          five = head $ filter (elem bseg) two_three_five
-          three = concat $ head $ uniq $ concat [two_three_five, [two, five]]
-          nine = head $ filter (notElem eseg) zero_six_nine
-          cseg = head $ filter (/=eseg) $  diff two [five]
-          six = head $ filter (notElem cseg) zero_six_nine
-          zero = concat $ head $ uniq $ concat [zero_six_nine, [six, nine]]
+          eight = getLen 7 leds
+          beseg = uniq $ concat two_three_five
+          eseg = head $ diff [beseg] four
+          bseg = head $ diff [beseg] [[eseg]]
+          two = filter (elem eseg) two_three_five
+          five = filter (elem bseg) two_three_five
+          three = uniq (two_three_five ++ two ++ five)
+          nine = filter (notElem eseg) zero_six_nine
+          cseg = head $ filter (/=eseg) $ diff two five
+          six = filter (notElem cseg) zero_six_nine
+          zero = uniq (zero_six_nine ++ six ++ nine)
 
 decode :: ([String], [String]) -> Int
 decode (segs, val) = read $ concat $ map pick val
