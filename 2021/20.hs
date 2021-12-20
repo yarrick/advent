6926e31b84728adb55e9666cf7db66f7c83b545d
Erik Ekman <erik@kryo.se>
2021-12-20 10:55:46 +0100

    Shorter day 20

--- 2021/20.hs
+++ 2021/20.hs
@@ -8,26 +8,23 @@ getxy m fallback (r,c)
 neighbors :: (Matrix Int,Int) -> (Int,Int) -> [Int]
 neighbors (m,def) (r,c) = [getxy m def (rr,cc) | rr <- [r-1..r+1], cc <- [c-1..c+1] ]
 
-enhance :: String -> (Matrix Int,Int) -> (Matrix Int,Int)
-enhance alg (m,def) = (matrix (nrows m) (ncols m) compute, compute (1,1))
-    where compute pos
-            | alg !! pval == '#' = 1
-            | otherwise = 0
+enhance :: [Int] -> (Matrix Int,Int) -> (Matrix Int,Int)
+enhance alg (m,def) = (matrix (nrows m) (ncols m) compute, compute (-10,-10))
+    where compute pos = alg !! pval
             where pval = foldl (\a b -> 2*a+b) 0 $ neighbors (m,def) pos
 
-process :: (String, (Matrix Int,Int)) -> [String]
-process (alg,(m,def)) = map show [lit 2, lit 50]
+process :: ([Int], (Matrix Int,Int)) -> [String]
+process (alg,(m,def)) = map (\n -> show$sum$toList$fst$steps !! n) [2, 50]
     where steps = iterate (enhance alg) (m,def)
-          lit n = sum $ toList $ fst $ steps !! n
 
-parse :: [String] -> (String, (Matrix Int,Int))
-parse (alg:_:state) = (alg, (matrix (extra + nrows mchar) (extra + ncols mchar) copier,0))
+parse :: [String] -> ([Int], (Matrix Int,Int))
+parse (alg:_:state) = (map adapt alg, (matrix (extra + nrows mchar) (extra + ncols mchar) copier,0))
     where mchar = fromLists state
           (extra,base) = (110, 55)
+          adapt c = length $ filter (=='#') [c]
           copier (r,c)
             | rr < 1 || rr > nrows mchar || cc < 1 || cc > ncols mchar = 0
-            | getElem rr cc mchar == '#' = 1
-            | otherwise = 0
+            | otherwise = adapt $ getElem rr cc mchar
             where (rr,cc) = (r - base, c - base)
 
 -- long file, lets do IO
