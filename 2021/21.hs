3441c08dadc8001d7ca4261b213063736a3b6c89
Erik Ekman <erik@kryo.se>
2021-12-21 15:09:25 +0100

    Day 21, 2021 - complete
    Runs in 12 minutes on one core.

--- 2021/21.hs
+++ 2021/21.hs
@@ -1,25 +1,61 @@
 import Data.List
+import Control.Monad
+import Control.DeepSeq
 
-type Player = (Int,Int) -- score, offset on board
+type Player = (Int,(Int,Int)) -- id, score, offset on board
 type Game = ([Player],(Int,[Int])) -- rolls done, dicevals
 
 turn :: Game -> Game
-turn (((sc,p):b:_),(rolls,dice)) = (b:(score,newpos):[],(rolls + 3, ndice))
-    where (rolled,ndice) = splitAt 3 dice
-          newpos = (p + sum rolled) `mod` 10
+turn (((n,(sc,p)):b:_),(rolls,(d:ds))) = (b:(n,(score,newpos)):[],(rolls + 3, ds))
+    where newpos = (p + d) `mod` 10
           score = newpos + 1 + sc
 
 play :: Int -> Game -> Game
 play lim g
+    | snd (snd g) == [] = g
     | mp >= lim = g
     | otherwise = play lim $ turn g
-    where mp = maximum $ map fst $ fst g
+    where mp = maximum $ map (fst.snd) $ fst g
 
 process :: [Int] -> [String]
-process offs = [show $ score $ play 1000 g]
-    where g = (zip (repeat 0) offs, (0, cycle [1..100]))
-          vis (ps,(dr,ds)) = show (ps, (dr, take 10 ds))
-          score gg = (minimum $ map fst $ fst gg) * (fst $ snd gg)
+process offs = map show [score $ play 1000 $ newg offs $ sumrolls $ cycle [1..100],
+                         maximum [p1,p2] ]
+    where score gg = (minimum $ map (fst.snd) $ fst gg) * (fst $ snd gg)
+          diracs = replicateM 5 sums
+          (p1,p2) = sumwins (0,0) $ try offs diracs
+
+newg :: [Int] -> [Int] -> Game
+newg offs d = (zip [1..] $ zip (repeat 0) offs, (0, d))
+
+sumrolls d = sum rolled : sumrolls next
+    where (rolled,next) = splitAt 3 d
+
+sumwins :: (Int,Int) -> [(Int,Int)] -> (Int,Int)
+sumwins r [] = r
+sumwins (a,b) ((w,rank):ws) = sumwins (deepseq newscore newscore) ws
+    where update pid sc
+            | w == pid = sc+rank
+            | otherwise = sc
+          newscore = (update 1 a, update 2 b)
+
+try :: [Int] -> [[(Int,Int)]] -> [(Int,Int)]
+try _ [] = []
+try offs (dc:dcs)
+    | res == [] = try offs expdc ++ try offs dcs
+    | otherwise = res ++ try offs dcs
+     where res = tryOne offs dc
+           expdc = [ dc ++ [a] | a <- sums ]
+
+tryOne :: [Int] -> [(Int,Int)] -> [(Int,Int)]
+tryOne offs ss
+    | length winner > 0 = [(fst $ head winner, product inst)]
+    | otherwise = []
+    where (rolls, inst) = unzip ss
+          res = play 21 $ newg offs rolls
+          winner = filter (\(_,(s,_)) -> s >= 21) $ fst res
+
+sums = map (\n -> (head n, length n)) $ group $ sort rolls
+    where rolls = [ a+b+c | a <- [1..3], b <- [1..3], c <- [1..3] ]
 
 parse :: [String] -> [Int]
 parse rows = map (pred . read . last . words) rows
