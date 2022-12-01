050885975c85a5c3087da289fa06fa7505ad167c
Erik Ekman <erik@kryo.se>
2022-12-01 10:03:27 +0100

    Day 1, 2022

---
+++ 2022/01.hs
@@ -0,0 +1,13 @@
+import Data.List
+
+chunk :: [String] -> [[Int]]
+chunk ss = map (map read) $ filter (/=[""]) $ groupBy (\x y -> length x > 0 && length y > 0) ss
+
+process :: [String] -> [String]
+process rows = map show [head elves, sum $ take 3 elves]
+    where elves = reverse $ sort $ map sum $ chunk rows
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . lines)
+
