3ec0ceda90667a7d6c2e1e46854194f0cc2e6686
Erik Ekman <erik@kryo.se>
2022-12-03 09:50:04 +0100

    Day 3, 2022

---
+++ 2022/03.hs
@@ -0,0 +1,23 @@
+import Data.Char
+import Data.List
+
+part1 :: String -> String
+part1 s = intersect (take l s) (drop l s)
+    where l = div (length s) 2
+
+part2 :: [String] -> [String]
+part2 [] = []
+part2 (a:b:c:cs) = (intersect a (intersect b c)) : part2 cs
+
+prio :: Char -> Int
+prio c
+    | c >= 'a' && c <= 'z' = 1 + (ord c) - (ord 'a')
+    | otherwise = 27 + (ord c) - (ord 'A')
+
+process :: [String] -> [String]
+process rows = map (show.sum.map (prio.head)) [map part1 rows, part2 rows]
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . lines)
+
