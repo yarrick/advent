9a52c3357fe8940f20bfa8f5d3544c7c810d1e88
Erik Ekman <erik@kryo.se>
2022-12-25 10:44:04 +0100

    Day 25, 2022

---
+++ 2022/25.hs
@@ -0,0 +1,38 @@
+num :: Char -> Int
+num '-' = -1
+num '=' = -2
+num c = read [c]
+
+snf :: Int -> Char
+snf (-2) = '='
+snf (-1) = '-'
+snf n = "012" !! n
+
+factors :: [Int]
+factors = map (\c -> 5^c) [0..]
+
+dec :: String -> Int
+dec cs = sum $ map (\(f,n) -> f * n) pairs
+    where fs = reverse $ take (length cs) factors
+          pairs = zip fs $ map num cs
+
+divs :: Int -> [Int] -> [Int]
+divs n [1] = [n]
+divs n (d:ds) = dv : divs rest ds
+    where (dv,rest) = divMod n d
+
+snaf n = map snf $ dropWhile (==0) $ reverse $ tune $ reverse $ divs n facts
+    where smaller = takeWhile (<n) factors
+          facts = reverse $ take (1 + length smaller) factors
+          tune [a] = [a]
+          tune (a:b:cs)
+            | a > 2 = (a-5) : tune ((b+1):cs)
+            | otherwise = a : tune (b:cs)
+
+process :: [String] -> [String]
+process rows = [snaf $ sum $ map dec rows]
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . lines)
+
