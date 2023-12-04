6fa4590140b3bac7afe9e534c538343e67b60847
Erik Ekman <erik@kryo.se>
2023-12-04 19:26:58 +0100

    Day 04, 2023

---
+++ 2023/04.hs
@@ -0,0 +1,23 @@
+import Data.Char
+import Data.List
+
+process :: [([Int], [Int])] -> [String]
+process cards = map (show.sum) [map pscore matches, play2 $ zip (cycle [1]) matches]
+    where matches = map (\(a,b) -> length $ intersect a b) cards
+          pscore 0 = 0
+          pscore n = 2 ^ (pred n)
+
+play2 :: [(Int,Int)] -> [Int]
+play2 [] = [0]
+play2 ((n,ms):ps) = n : play2 rest
+    where rest = (map (\(a,b) -> (a+n,b)) $ take ms ps) ++ (drop ms ps)
+
+parse :: String -> ([Int], [Int])
+parse str = (map read wins, map read $ tail nums)
+    where (_:_:ns) = words str
+          (wins,nums) = break ("|"==) ns
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . map parse . lines)
+
