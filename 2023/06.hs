896f286e046c28be466b3e6ddeb8bfdf7a14ef20
Erik Ekman <erik@kryo.se>
2023-12-06 07:42:16 +0100

    Day 06, 2023
    Runs in 2.2s, or 54ms with -O2

---
+++ 2023/06.hs
@@ -0,0 +1,14 @@
+process :: [String] -> [String]
+process rows = map (show . run) [p1, [[t2],[d2]] ]
+    where p1 = map (map read . tail . words) rows
+          run (t:d:_) = product $ map race $ zip t d
+          (t2:d2:[]) = map (read . concat . tail . words) rows
+
+race :: (Int, Int) -> Int
+race (t,d) = length $ filter (\k -> (t - k) * k > d) dists
+    where dists = [1..t]
+
+-- long file, lets do IO
+main :: IO ()
+main = interact (unlines . process . lines)
+
