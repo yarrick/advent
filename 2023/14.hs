4f4387c7c8612abd3304f63846255a1af580b19c
Erik Ekman <erik@kryo.se>
2023-12-15 21:04:53 +0100

    Day 14, 2023 - complete

--- 2023/14.hs
+++ 2023/14.hs
@@ -1,13 +1,26 @@
 import Data.List
+import Data.Maybe
 
 process :: [String] -> [String]
-process rows = map (show.sum) [map score $ zip [1..(length rows)] $ reverse fallen]
-    where fallen = transpose $ map fall $ transpose rows
-          score (n,s) = n * length (filter ('O'==) s)
+process dish = map (show.sum.load) [north dish, spun !! (loop1 + cycles)]
+    where score (n,s) = n * length (filter ('O'==) s)
+          load s = map score $ zip [1..] $ reverse s
+          spun = iterate (east . south . west . north) dish
+          loops old ((n,d):ds)
+            | isJust ix = (n, (fromJust ix)) : loops [d] ds
+            | otherwise = loops (old++[d]) ds
+            where ix = elemIndex d old
+          (loop1,repeated):(loop2,_):_ = loops [] $ zip [0..] spun
+          cycles = (1000000000 - loop1) `mod` (loop2 - loop1)
 
-fall :: String -> String
-fall [] = []
-fall s = reverse (sort free) ++ (take 1 stuck) ++ fall (drop 1 stuck)
+north = transpose . west . transpose
+south = reverse . north . reverse
+west = map (reverse . right . reverse)
+east = map right
+
+right :: String -> String
+right [] = []
+right s = (sort free) ++ (take 1 stuck) ++ right (drop 1 stuck)
     where (free,stuck) = break ('#'==) s
 
 main :: IO ()
