d1bd0f61d639014791ac51553e5626df77657e6e
Erik Ekman <erik@kryo.se>
2023-12-15 23:12:31 +0100

    Day 12, 2023 - done
    Runs in 0.7s

--- 2023/12.hs
+++ 2023/12.hs
@@ -1,43 +1,56 @@
 import Data.List
+import Data.Maybe
+import qualified Data.Map as M
+
+-- matched ranges, unmatched ranges, string segment, coming segments
+type Candidate = ([Int], [Int], String, [String])
 
 process :: [String] -> [String]
-process rows = map show [build 1, build 5]
-    where build n = sum $ concatMap (run.parse n) rows
+process rows = map show [run 1, run 5]
+    where run n = sum $ map (snd.(segment M.empty).parse n) rows
 
-run ws = map score done ++ concatMap run more
-    where end (a,b,c) = a == [] && b == []
-          (done, more) = partition end $ paths ws
-          score (_,_,w) = w
 
-paths (s:ss, ds, w) = filter valid outs
-    where cands = group $ sort $ filter (\p -> not $ elem (-1) (map fst p)) $ candidates ds s sslen
-          sslen = sum $ intersperse 1 $ map length ss
-          outs = map (\(p:ps) -> (ss, drop (length p) ds, w * length (p:ps))) cands
-          valid (pattern,nums,_)
-            | elem '#' (concat pattern) && nums == [] = False
-            | pattern == [] && length nums > 0 = False
-            | otherwise = True
+segment :: M.Map Candidate Int -> Candidate -> (M.Map Candidate Int, Int)
+segment m seg@(prev,cur,s,ss)
+    | cur == [] && s == "" && ss == [] = (m, 1)
+    | isJust cached = (m, fromJust cached)
+    | otherwise = (M.insert seg combs newm, combs)
+    where cached = M.lookup seg m
+          gcount cs = map (\a -> (length a, head a)) $ group $ sort cs
+          cands = gcount $ concatMap step $ cand2 seg
+          (newm, combs) = foldl combinations (m,0) cands
 
-candidates :: [Int] -> String -> Int -> [[(Int,String)]]
-candidates [] s _
-    | elem '#' s = [[(-1,s)]]
-    | otherwise = [[]]
-candidates (len:next) s sslen
-    | elem '#' s && length s < len = [[(-1,s)]]
-    | length s < len = [[]]
-    | slack < 0 = [[(-1,s)]]
-    | head s == '#' && length rest > 0 && head rest == '#' = [[(-1,s)]]
-    | head s == '#' = found
-    | length rest > 0 && head rest == '#' = candidates (len:next) (tail s) sslen
-    | elem '#' rest && next == [] = candidates (len:next) (tail s) sslen
-    | elem '#' rest && length rest <= (head next) = candidates (len:next) (tail s) sslen
-    | otherwise = found ++ candidates (len:next) (tail s) sslen
+combinations :: (M.Map Candidate Int, Int) -> (Int, Candidate) -> (M.Map Candidate Int, Int)
+combinations (m,cnt) (mul,cand) = (newm, cnt + (mul * combs))
+    where (newm,combs) = segment m cand
+
+step :: Candidate -> [Candidate]
+step c@(prev,cur,s,ss)
+    | length totstring < sum cur = []
+    | cur == [] && elem '#' totstring = []
+    | cur == [] = [(prev, cur, "", [])]
+    | elem '#' s && length s < head cur = []
+    | length s < head cur = [(prev, cur, head ss, tail ss)]
+    | otherwise = [c]
+    where totstring = concat $ s:ss
+
+cand2 :: Candidate -> [Candidate]
+cand2 (prev,len:next,s,ss)
+    | elem '#' s && length s < len = []
+    | length s < len = [(prev,len:next,s,ss)]
+    | slack < 0 = []
+    | head s == '#' && length rest > 0 && head rest == '#' = []
+    | head s == '#' = [(prev++[len],next,drop 1 rest,ss)]
+    | length rest > 0 && head rest == '#' = cand2 (prev, len:next, tail s, ss)
+    | elem '#' rest && next == [] = cand2 (prev, len:next, tail s, ss)
+    | elem '#' rest && length rest <= (head next) = cand2 (prev, len:next, tail s, ss)
+    | otherwise = found ++ cand2 (prev, len:next, tail s, ss)
     where (cand,rest) = splitAt len s
-          slack = (length s) + 1 + sslen - (sum $ intersperse 1 (len:next))
-          found = map ([(len,cand)]++) $ candidates next (drop 1 rest) sslen
+          slack = foldl1 (-) $ map (sum . intersperse 1) [map length (s:ss), len:next]
+          found = [(prev++[len],next,drop 1 rest,ss)]
 
-parse :: Int -> String -> ([String], [Int], Int)
-parse n row = (chunk [] $ concat $ take (pred $ n*2) springlist, concat $ take n numlist, 1)
+parse :: Int -> String -> Candidate
+parse n row = ([], concat $ take n numlist , head chunks, drop 1 chunks)
     where (springs:list:[]) = words row
           springlist = intersperse "?" $ cycle [springs]
           numlist = cycle [read $ "[" ++ list ++ "]"]
@@ -47,6 +60,7 @@ parse n row = (chunk [] $ concat $ take (pred $ n*2) springlist, concat $ take n
             | c == '.' && g /= [] = g : chunk [] as
             | c == '.' = chunk [] as
             | otherwise = chunk (g++[c]) as
+          chunks = chunk [] $ concat $ take (pred $ n*2) springlist
 
 main :: IO ()
 main = interact (unlines . process . lines)
