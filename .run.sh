f0ba820feaaff8969bd46c2a443aef2df1c706c0
Erik Ekman <erik@kryo.se>
2023-12-16 14:30:48 +0100

    Unify indents

--- .run.sh
+++ .run.sh
@@ -6,5 +6,5 @@ do
     dir=$(dirname $f)
     file=$(basename $f)
     echo "Running with $f as input"
-	time $BIN +RTS -M10g -s.rts_stats_${dir}_${file}.log < $f
+    time $BIN +RTS -M10g -s.rts_stats_${dir}_${file}.log < $f
 done
