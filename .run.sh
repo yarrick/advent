623ed812f105b93a254d21bdd3af88c20b92d829
Erik Ekman <erik@kryo.se>
2023-12-16 14:27:20 +0100

    Update makefile, store examples
    Store inputs in unique files to not have to re-fetch them later.
    Keep them in an ignored directory to avoid submitting them.
    
    `make` now builds the latest touched day-file.
    `make test` runs all example files for it.
    `make run` runs the input file (and fetches it if neccessary).

---
+++ .run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+BIN=$1
+shift 1
+for f in "$@"
+do
+    dir=$(dirname $f)
+    file=$(basename $f)
+    echo "Running with $f as input"
+	time $BIN +RTS -M10g -s.rts_stats_${dir}_${file}.log < $f
+done
