3fad0e060cf23ff0c8a3dba5e95cf7993d5f3e66
Erik Ekman <erik@kryo.se>
2023-12-16 15:55:57 +0100

    Add main method

--- 2015/11.hs
+++ 2015/11.hs
@@ -1,11 +1,10 @@
-
 straight3 :: String -> Bool
 straight3 [] = False
 straight3 (a:[]) = False
 straight3 (a:b:[]) = False
 straight3 (a:b:c:dd)
-	| b == succ a && c == succ b = True
-	| otherwise = straight3 (b:c:dd)
+    | b == succ a && c == succ b = True
+    | otherwise = straight3 (b:c:dd)
 
 noiol :: String -> Bool
 noiol str = not (elem 'i' str || elem 'o' str || elem 'l' str)
@@ -14,8 +13,8 @@ pairs :: String -> Int
 pairs [] = 0
 pairs (a:[]) = 0
 pairs (a:b:cc)
-	| a == b = 1 + pairs cc
-	| otherwise = pairs (b:cc)
+    | a == b = 1 + pairs cc
+    | otherwise = pairs (b:cc)
 
 goodpassword :: String -> Bool
 goodpassword str = straight3 str && noiol str && pairs str >= 2
@@ -23,13 +22,19 @@ goodpassword str = straight3 str && noiol str && pairs str >= 2
 step :: String -> String
 step [] = []
 step (a:bb)
-	| a == 'z' = 'a' : step bb
-	| otherwise = succ a : bb
+    | a == 'z' = 'a' : step bb
+    | otherwise = succ a : bb
 
 next :: String -> String
 next str = reverse $ step $ reverse str
 
 run :: String -> String
 run str
-	| goodpassword str = str
-	| otherwise = run $ next str
+    | goodpassword str = str
+    | otherwise = run $ next str
+
+process (row:_) = [part1, run $ next part1]
+    where part1 = run row
+
+main :: IO ()
+main = interact (unlines . process . lines)
--- 2015/25.hs
+++ 2015/25.hs
@@ -1,9 +1,17 @@
+import Data.Char
+
 pos :: Integer -> Integer -> Integer
 pos row col = foldl (+) colstart $ takex row col
-	where
-		takex num start = take (fromInteger (num - 1)) [start..]
-		colstart = foldl (+) 1 $ takex col 2
+    where
+        takex num start = take (fromInteger (num - 1)) [start..]
+        colstart = foldl (+) 1 $ takex col 2
 
 calc :: Integer -> Integer -> Integer
 calc row col = (20151125 * ((252533 ^ num) `mod` 33554393)) `mod` 33554393
-	where num = (pos row col) - 1
+    where num = (pos row col) - 1
+
+process (row:_) = [show $ calc a b]
+    where (a:b:_) = map read $ filter (""/=) $ map (filter isDigit) $ words row
+
+main :: IO ()
+main = interact (unlines . process . lines)
