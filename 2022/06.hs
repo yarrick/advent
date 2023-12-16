a95886b6f6d31d4a44ca5d20e2c48e42d34fa2bc
Erik Ekman <erik@kryo.se>
2023-12-17 00:01:20 +0100

    Add main method

--- 2022/06.hs
+++ 2022/06.hs
@@ -5,6 +5,12 @@ frame prev len (s:ss)
     | length (nub (s:prev)) == length (s:prev) = succ len
     | otherwise = frame (tail prev ++ [s]) (succ len) ss
 
-run :: String -> (Int, Int)
-run s = (solve 3, solve 13)
+run :: String -> [Int]
+run s = [solve 3, solve 13]
     where solve sl = frame (take sl s) sl (drop sl s)
+
+process :: [String] -> [String]
+process (row:_) = map show $ run row
+
+main :: IO ()
+main = interact (unlines . process . lines)
