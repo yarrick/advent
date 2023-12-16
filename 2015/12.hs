38b2c832d0332c39900313e929b4a0efdb7e5033
Erik Ekman <erik@kryo.se>
2023-12-16 16:07:50 +0100

    Add main method

--- 2015/12.hs
+++ 2015/12.hs
@@ -1,13 +1,13 @@
 import Data.Char (isDigit, isLetter)
 
 data Tok = ListStart | ListEnd | ObjStart | ObjEnd | Comma | Colon | TString String | TInt Int
-	deriving Show
+    deriving Show
 
 isNotNum :: Char -> Bool
 isNotNum c = not $ isDigit c || c == '-'
 
 isNotStr :: Char -> Bool
-isNotStr c = not $ isLetter c || c == '\''
+isNotStr c = not $ isLetter c || c == '"'
 
 tokenize :: String -> [Tok]
 tokenize [] = []
@@ -17,10 +17,10 @@ tokenize ('{':str) = ObjStart : tokenize str
 tokenize ('}':str) = ObjEnd : tokenize str
 tokenize (',':str) = Comma : tokenize str
 tokenize (':':str) = Colon : tokenize str
-tokenize str@('\'':_) = (TString word) : tokenize end
-	where (word, end) = break isNotStr str
+tokenize str@('\"':_) = (TString word) : tokenize end
+    where (word, end) = break isNotStr str
 tokenize str = (TInt $ read num) : tokenize end
-	where (num, end) = break isNotNum str
+    where (num, end) = break isNotNum str
 
 data Js = JsList [Js] | JsObj [Js] | JsInt Int | JsString String deriving Show
 
@@ -31,9 +31,9 @@ parseList (TString str:tokens) js = parseList tokens (js ++ [JsString str])
 parseList (TInt num:tokens) js = parseList tokens (js ++ [JsInt num])
 parseList (Comma:tokens) js = parseList tokens js
 parseList (ListStart:listtok) js = parseList tokens (js ++ [sublist])
-	where (sublist, tokens) = parseList listtok []
+    where (sublist, tokens) = parseList listtok []
 parseList (ObjStart:objtok) js = parseList tokens (js ++ [obj])
-	where (obj, tokens) = parseObj objtok []
+    where (obj, tokens) = parseObj objtok []
 
 parseObj :: [Tok] -> [Js] -> (Js, [Tok])
 parseObj (ObjEnd:tokens) js = (JsObj js, tokens)
@@ -42,15 +42,15 @@ parseObj (TInt num:tokens) js = parseObj tokens (js ++ [JsInt num])
 parseObj (Comma:tokens) js = parseObj tokens js
 parseObj (Colon:tokens) js = parseObj tokens js
 parseObj (ListStart:listtok) js = parseObj tokens (js ++ [sublist])
-	where (sublist, tokens) = parseList listtok []
+    where (sublist, tokens) = parseList listtok []
 parseObj (ObjStart:objtok) js = parseObj tokens (js ++ [obj])
-	where (obj, tokens) = parseObj objtok []
+    where (obj, tokens) = parseObj objtok []
 
 parse :: [Tok] -> Js
 parse (ListStart:listtok) = list
-	where (list, tokens) = parseList listtok []
+    where (list, tokens) = parseList listtok []
 parse (ObjStart:objtok) = obj
-	where (obj, tokens) = parseObj objtok []
+    where (obj, tokens) = parseObj objtok []
 
 sumJs :: Int -> Js -> Int
 sumJs x (JsInt y) = x + y
@@ -58,12 +58,9 @@ sumJs x (JsString _) = x
 sumJs x (JsList l) = x + foldl sumJs 0 l
 sumJs x (JsObj o) = x + foldl sumJs 0 o
 
--- convert " to ' in inputfile to not mess up string argument
-run str = sumJs 0 $ parse $ tokenize str
-
 --part 2
 isred :: Js -> Bool
-isred (JsString "'red'") = True
+isred (JsString "\"red\"") = True
 isred _ = False
 
 nored :: Js -> Js
@@ -71,7 +68,11 @@ nored val@(JsInt i) = val
 nored val@(JsString s) = val
 nored (JsList list) = JsList (map nored list)
 nored (JsObj objs)
-	| any isred objs = JsObj []
-	| otherwise = JsObj (map nored objs)
+    | any isred objs = JsObj []
+    | otherwise = JsObj (map nored objs)
 
-run2 str = sumJs 0 $ nored $ parse $ tokenize str
+process row = map (show.sumJs 0) [parsed, nored parsed]
+    where parsed = parse $ tokenize row
+
+main :: IO ()
+main = interact (unlines . process)
