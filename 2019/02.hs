e3b25371b86f9e7ec239cec4192e3f0a864c3616
Erik Ekman <erik@kryo.se>
2023-12-17 23:27:07 +0100

    Handle part 1 example input for part 2

--- 2019/02.hs
+++ 2019/02.hs
@@ -12,5 +12,14 @@ run bytes = memzero $ exec $ newstate (program 1202 $ parse bytes) []
 
 -- part 2
 
-run2 bytes = fst $ head $ filter (\a -> snd a == 19690720) $
-  map (\prog -> (prog, memzero (exec $ newstate (program prog $ parse bytes) []))) [0..9999]
+run2 bytes
+    | matches == [] = 0
+    | otherwise = head matches
+    where matches = map snd $ take 1 $ filter (\a -> snd a == 19690720) $
+                    map (\prog -> (prog, memzero (exec $ newstate (program prog $ parse bytes) []))) [0..9999]
+
+process :: String -> [String]
+process rows = [show $ run rows, show $ run2 rows]
+
+main :: IO ()
+main = interact (unlines . process)
