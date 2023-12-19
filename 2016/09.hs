32b15b2bd59c6e8dfcf73e4cc6600e81f7a18329
Erik Ekman <erik@kryo.se>
2023-12-19 09:41:59 +0100

    Run both parts

--- 2016/09.hs
+++ 2016/09.hs
@@ -12,12 +12,6 @@ expand ('(':bb) = (rep * (length repstr)) + expand nstr
         nstr = drop count $ tail tt
 expand (_:bb) = 1 + expand bb
 
-process :: [String] -> [String]
-process rows = map (show . expand) rows
-
-main :: IO ()
-main = interact (unlines . process2 . lines)
-
 -- part 2
 
 expand2 :: String -> Int
@@ -30,6 +24,8 @@ expand2 ('(':bb) = expand2 expanded + expand2 nstr
         expanded = take (count*rep) (cycle repstr)
 expand2 (_:bb) = 1 + expand2 bb
 
-process2 :: [String] -> [String]
-process2 rows = map (show . expand2) rows
+process :: [String] -> [String]
+process (row:_) = map show [expand row, expand2 row]
 
+main :: IO ()
+main = interact (unlines . process . lines)
