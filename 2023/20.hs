e52e3e7582e0a29aefb8223600478671faa29935
Erik Ekman <erik@kryo.se>
2024-01-02 14:41:55 +0100

    Day 20 complete

--- 2023/20.hs
+++ 2023/20.hs
@@ -1,19 +1,13 @@
 import Data.Char
 import Data.List
 import qualified Data.Map as M
-import Control.DeepSeq
-import Debug.Trace
+import Data.Maybe
 
 data Pulse = Low | High deriving (Eq, Ord, Show)
-instance NFData Pulse where rnf x = seq x ()
 
 data Module = FlipFlop Bool [String] |
               Conj [(String, Pulse)] [String] |
               Broadcast [String] deriving (Eq, Show)
-instance NFData Module where
-    rnf (FlipFlop st dest) = st `deepseq` dest `deepseq` ()
-    rnf (Conj hist dest) = hist `deepseq` dest `deepseq` ()
-    rnf x = seq x ()
 
 tick :: Module -> (String, String, Pulse) -> (Module, [(String, String, Pulse)])
 tick (Broadcast dest) (from,to,p) = (Broadcast dest, [(to,d,p) | d <- dest])
@@ -30,14 +24,31 @@ dests (n, (FlipFlop _ dest)) = map (\d -> (d,n)) dest
 dests (n ,(Conj _ dest)) = map (\d -> (d,n)) dest
 dests (n, (Broadcast dest)) = map (\d -> (d,n)) dest
 
+rxlow _ _ [] = "Nothing sends to 'rx'"
+rxlow mods ((Conj hist dest):[]) (name:[]) = show $ product $ firsthighs sources name mods
+    where sources = map fst hist
+rxlow _ _ _ = "Module sending to 'rx' not Conjunction type"
+
+firsthighs :: [String] -> String -> M.Map String Module -> [Int]
+firsthighs srcs goal mods = map (fst.fromJust.(\s -> find (\(_,ps) -> elem s (map sender ps)) stream)) srcs
+    where stream = loops srcs goal (mods,1)
+          sender (se,_,_) = se
+
+loops :: [String] -> String -> (M.Map String Module, Int) -> [(Int,[(String,String,Pulse)])]
+loops srcs goal (mods, it)
+    | length his > 0 = (it,his) : loops srcs goal (ns,succ it)
+    | otherwise = loops srcs goal (ns,succ it)
+    where (ns,np) = step (mods,[]) [("button","broadcaster",Low)]
+          his = filter (\(from,to,p) -> elem from srcs && to == goal && p == High) np
+
 process :: [(String, Module)] -> [String]
-process mods = [show $ product $ map length $ group $ sort $ map snd p, show rxticks]
+process mods = [show $ product pcount, rxlow (fst start) rxmod rxsrc]
     where ds = groupBy (\a b -> fst a == fst b) $ sort $ concatMap dests mods
           start = (M.fromList $ prep mods ds, [])
           (m,p) = foldl step start (replicate 1000 [("button","broadcaster",Low)])
-          rxticks
-            | elem "rx" (map fst $ concatMap dests mods) = speedstep (fst start,1)
-            | otherwise = 0
+          pcount = map length $ group $ sort $ map (\(_,_,pu) -> pu) p
+          rxsrc = map snd $ concat $ filter (\n -> fst (head n) == "rx") ds
+          rxmod = map ((fst start) M.!) rxsrc
 
 prep :: [(String,Module)] -> [[(String,String)]] -> [(String,Module)]
 prep [] _ = []
@@ -45,18 +56,11 @@ prep ((n,(Conj a dest)):ms) ds = (n,(Conj (map (\t -> (t,Low)) dests) dest)) : p
     where dests = map snd $ head $ filter (\d -> fst (head d) == n) ds
 prep (m:ms) ds = m : prep ms ds
 
-speedstep :: (M.Map String Module,Int) -> Int
-speedstep (mods, it)
-    | elem ("rx",Low) np = it
-    | it `mod` 100000 == 0 = trace (show it) deepseq (ns,it) $ speedstep (ns,succ it)
-    | otherwise = deepseq (ns,it) $ speedstep (ns,succ it)
-    where (ns,np) = step (mods,[]) [("button","broadcaster",Low)]
-
-step :: (M.Map String Module, [(String,Pulse)]) -> [(String, String, Pulse)] -> (M.Map String Module, [(String,Pulse)])
+step :: (M.Map String Module, [(String,String,Pulse)]) -> [(String, String, Pulse)] -> (M.Map String Module, [(String,String,Pulse)])
 step a [] = a
 step (mods, past) ((from,to,p):ps)
-    | M.notMember to mods = step (mods,(to,p):past) ps
-    | otherwise = step (M.insert to nm mods, (to,p):past)  (ps ++ np)
+    | M.notMember to mods = step (mods,(from,to,p):past) ps
+    | otherwise = step (M.insert to nm mods, (from,to,p):past) (ps ++ np)
     where (nm,np) = tick (mods M.! to) (from,to,p)
 
 parse :: String -> (String, Module)
