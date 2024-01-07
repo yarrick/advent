8bdae032bd23689c7753f490547419e203a9360e
Erik Ekman <erik@kryo.se>
2024-01-07 18:06:02 +0100

    Switch to map for duplicate check

--- 2018/01.hs
+++ 2018/01.hs
@@ -1,19 +1,20 @@
+import qualified Data.Map as M
+
 parse :: String -> (Int -> Int)
 parse (c:num)
-  | c == '+' = (+n)
-  | c == '-' = (+(-n))
-  where n = read num
+    | c == '+' = (+n)
+    | c == '-' = (+(-n))
+    where n = read num
 
-firstdup :: [Int] -> [Int] -> Int
+firstdup :: M.Map Int Int -> [Int] -> Int
 firstdup seen (x:xs)
-  | elem x seen = x
-  | otherwise = firstdup (x:seen) xs
+    | M.member x seen = x
+    | otherwise = firstdup (M.insert x 1 seen) xs
 
-process :: [String] -> [String]
-process rows = [show $ foldl apply 0 ops,
-                show $ firstdup [] $ scanl apply 0 $ cycle ops ]
-  where apply n fn = fn n
-        ops = map parse rows
+process :: [(Int -> Int)] -> [String]
+process ops = map show [foldl apply 0 ops,
+                        firstdup M.empty $ scanl apply 0 $ cycle ops ]
+    where apply n fn = fn n
 
 main :: IO ()
-main = interact (unlines . process . lines)
+main = interact (unlines . process . map parse . lines)
