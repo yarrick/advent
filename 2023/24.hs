d35476a3bc28502cc13050bf4f00b62c5fbfb356
Erik Ekman <erik@kryo.se>
2024-01-12 23:58:29 +0100

    Shorter part 1

--- 2023/24.hs
+++ 2023/24.hs
@@ -1,52 +1,31 @@
-import Data.List
-
-type Xyz = (Int, Int, Int)
-type Point = (Xyz, Xyz)
 type Segment = ((Int,Int), (Int,Int))
 
 cross :: (Segment, Segment) -> (Float, Float)
 cross (((x1,y1),(x2,y2)), ((x3,y3),(x4,y4))) =
     (frac ((x1-x3)*(y3-y4) - (y1-y3)*(x3-x4)), frac ((x1-x3)*(y1-y2) - (y1-y3)*(x1-x2)))
-      where frac c = fdiv c ((x1-x2)*(y3-y4) - (y1-y2)*(x3-x4))
+      where frac c = (fromIntegral c) / (fromIntegral ((x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)))
 
-crossing :: (Int,Int) -> [(Segment,Segment)] -> [((Segment,Segment),(Float,Float))]
-crossing _ [] = []
-crossing (lo,hi) (s:ss)
-    | t < 0.0 || u < 0.0 = crossing (lo,hi) ss
-    | xc < fromIntegral lo || xc > fromIntegral hi = crossing (lo,hi) ss
-    | yc < fromIntegral lo || yc > fromIntegral hi = crossing (lo,hi) ss
-    | otherwise = (s,(xc,yc)): crossing (lo,hi) ss
+crossing :: (Float, Float) -> (Segment, Segment) -> Bool
+crossing (lo,hi) s = t >= 0.0 && u >= 0.0 && minimum xy >= lo && maximum xy <= hi
     where (t,u) = cross s
-          (xc,yc) = cpoint s t
+          xy = cpoint s t
 
-cpoint :: (Segment,Segment) -> Float -> (Float,Float)
-cpoint (((x1,y1),(x2,y2)),_) ratio = (p x1 x2, p y1 y2)
-    where p st en = (fromIntegral st) + (fromIntegral (en - st))  * ratio
+cpoint :: (Segment, Segment) -> Float -> [Float]
+cpoint (((x1,y1),(x2,y2)),_) ratio = [p x1 x2, p y1 y2]
+    where p st en = (fromIntegral st) + (fromIntegral (en - st)) * ratio
 
-fdiv :: Int -> Int -> Float
-fdiv a b = (fromIntegral a) / (fromIntegral b)
-
-pairs :: [a] -> [(a,a)]
-pairs [a] = []
-pairs (a:bs) = [(a,b) | b <- bs] ++ pairs bs
-
-xylineseg :: Point -> Segment
-xylineseg ((x,y,z), (vx,vy,vz)) = ((x,y), (x+vx,y+vy))
-
-step :: Point -> Point
-step ((x,y,z), (vx,vy,vz)) = ((x+vx,y+vy,z+vz), (vx,vy,vz))
-
-process :: [Point] -> [String]
-process ps = [ show $ length xycross ]
+process :: [[(Int, Int)]] -> [String]
+process ps = [ show $ length $ filter (crossing box) $ pairs $ map xylineseg ps ]
     where box
-            | length ps < 10 = (7, 27) -- For example data
-            | otherwise = (200000000000000, 400000000000000)
-          xycross = crossing box $ pairs $ map xylineseg ps
+            | length ps < 10 = (7.0, 27.0) -- For example data
+            | otherwise = (200000000000000.0, 400000000000000.0)
+          xylineseg ((x,vx):(y,vy):_) = ((x,y), (x+vx,y+vy))
+          pairs [a] = []
+          pairs (a:bs) = [(a,b) | b <- bs] ++ pairs bs
 
-parse :: String -> Point
-parse r = ((x,y,z), (ax,ay,az))
-    where parts = map (filter (','/=)) $ words r
-          (x:y:z:_:ax:ay:az:_) = map read parts
+parse :: String -> [(Int, Int)]
+parse r = [(x,ax),(y,ay),(z,az)]
+    where (x:y:z:_:ax:ay:az:_) = map (read.filter (','/=)) $ words r
 
 main :: IO ()
 main = interact (unlines . process . map parse . lines)
