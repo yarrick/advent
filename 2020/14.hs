9001b4f9b5cb6a1687691b689698f2f35d8fb979
Erik Ekman <erik@kryo.se>
2024-01-20 12:51:52 +0100

    Use map, massively faster
    Avoid using all RAM in the world on the first example in part 2.
    
    Add second example.

--- 2020/14.hs
+++ 2020/14.hs
@@ -1,25 +1,26 @@
 import Data.Bits
 import Data.Char
 import Data.List
+import qualified Data.Map as M
 
-data Cmd = Mask String | Write Integer Integer deriving (Eq,Show)
+data Cmd = Mask String | Write Int Int deriving (Eq,Show)
 
-asNum :: (Char,Integer,Integer) -> String -> Integer
+asNum :: (Char,Int,Int) -> String -> Int
 asNum _ [] = 0
 asNum n@(a,an,bn) (c:cs)
     | c == a = an + 2 * (asNum n cs)
     | otherwise = bn + 2 * (asNum n cs)
 
-asBin :: Integer -> [Integer]
+asBin :: Int -> [Int]
 asBin n = (take (36 - length res) $ cycle [0]) ++ res
     where conv 0 = []
           conv n = (mod n 2) : conv (div n 2)
           res = (reverse (conv n))
 
-valMask :: String -> Integer -> Integer
+valMask :: String -> Int -> Int
 valMask m n = (n .|. (asNum ('1',1,0) $ reverse m)) .&. (asNum ('0',0,1) $ reverse m)
 
-addrMask :: String -> [Integer] -> [[Integer]]
+addrMask :: String -> [Int] -> [[Int]]
 addrMask [] [] = [[]]
 addrMask ('0':mm) (p:ps) = map (\l -> p : l) $ addrMask mm ps
 addrMask ('1':mm) (p:ps) = map (\l -> 1 : l) $ addrMask mm ps
@@ -29,23 +30,23 @@ parse :: [String] -> Cmd
 parse ("mask":"=":mm:xs) = Mask mm
 parse (wr:"=":num:xs) = Write (read $ filter isDigit wr) (read num)
 
-run :: [Cmd] -> String -> [(Integer,Integer)] -> [(Integer,Integer)]
-run [] _ a = a
+run :: [Cmd] -> String -> M.Map Int Int -> [Int]
+run [] _ a = M.elems a
 run ((Mask mm):cs) _ mem = run cs mm mem
-run ((Write pos val):cs) currmask mem = run cs currmask $ (pos,mval) : omem
+run ((Write pos val):cs) currmask mem = run cs currmask $ M.insert pos mval mem
     where mval = valMask currmask val
-          omem = filter (\(p,_) -> p /= pos) mem
 
-run2 :: [Cmd] -> String -> [(String,Integer)] -> [(String,Integer)]
-run2 [] _ a = a
-run2 ((Mask mm):cs) _ mem = run2 cs mm mem
-run2 ((Write pos val):cs) currmask mem = run2 cs currmask (newmem++oldmem)
+run2 :: [Cmd] -> String -> M.Map String Int -> [Int]
+run2 [] _ a = M.elems a
+run2 ((Mask mm):cs) _ mem
+    | length (filter ('X'==) mm) > 20 = [] -- avoid exploding on first example
+    | otherwise = run2 cs mm mem
+run2 ((Write pos val):cs) currmask mem = run2 cs currmask $ foldl update mem poss
     where poss = map (concatMap show) $ addrMask currmask $ asBin pos
-          newmem = zip poss (cycle [val])
-          (_,oldmem) = partition (\(k,_) -> elem k poss) mem
+          update m k = M.insert k val m
 
 process :: [String] -> [String]
-process rows = map (show.sum) [map snd $ run cmds "X" [], map snd $ run2 cmds "X" [] ]
+process rows = map (show.sum) [run cmds "" M.empty, run2 cmds "" M.empty]
     where cmds = map (parse.words) rows
 
 main :: IO ()
