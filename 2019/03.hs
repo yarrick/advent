a973b32c0c6e9815173daa446a9717b38a1fb1c7
Erik Ekman <erik@kryo.se>
2024-01-21 15:11:20 +0100

    Get both answers from second part calculation

--- 2019/03.hs
+++ 2019/03.hs
@@ -1,77 +1,45 @@
+import Prelude hiding (Left,Right)
 import Data.List
+
+data Direction = Up | Down | Left | Right deriving (Eq, Show)
 
 parse :: String -> [(Direction,Int)]
 parse [] = []
 parse w = (dir $ head move, read $ tail move) : parse (drop 1 end)
     where (move, end) = break (','==) w
-
-data Direction = GoUp | GoDown | GoLeft | GoRight deriving (Eq, Show)
-
-dir :: Char -> Direction
-dir 'U' = GoUp
-dir 'D' = GoDown
-dir 'R' = GoRight
-dir 'L' = GoLeft
+          dir c = snd $ head $ filter (\f -> fst f == c) [('U',Up),('D',Down),('R',Right),('L',Left)]
 
 expand :: Int -> Int -> [(Direction,Int)] -> [(Int,Int)]
 expand _ _ [] = []
 expand x y ((d,len):rest) = zip xlist ylist ++ expand (last xlist) (last ylist) rest
   where
-    xlist = xpos x d len
-    ylist = ypos y d len
+    xlist = dpos ([Up,Down],Left,Right) x d len
+    ylist = dpos ([Left,Right],Down,Up) y d len
 
-xpos :: Int -> Direction -> Int -> [Int]
-xpos start dir len
-  | dir == GoUp || dir == GoDown = replicate len start
-  | dir == GoLeft = map (\a -> start - a) steps
-  | dir == GoRight = map (\a -> start + a) steps
+dpos :: ([Direction],Direction,Direction) -> Int -> Direction -> Int -> [Int]
+dpos (zero,neg,pos) start dir len
+  | elem dir zero = replicate len start
+  | dir == neg = map (\a -> start - a) steps
+  | dir == pos = map (\a -> start + a) steps
     where steps = take len [1..]
-
-ypos :: Int -> Direction -> Int -> [Int]
-ypos start dir len
-  | dir == GoLeft || dir == GoRight = replicate len start
-  | dir == GoDown = map (\a -> start - a) steps
-  | dir == GoUp = map (\a -> start + a) steps
-    where steps = take len [1..]
-
-wire :: String -> [(Int,Int)]
-wire moves = sort $ expand 0 0 $ parse moves
-
-run :: String -> String -> Int
-run a b = minimum $ map (\(x,y) -> (abs x) + (abs y)) $ cross (wire a) (wire b)
-
--- like List intersect but for sorted lists
-cross :: [(Int,Int)] -> [(Int,Int)] -> [(Int,Int)]
-cross [] _ = []
-cross _ [] = []
-cross x@((a,b):xs) y@((c,d):ys)
-  | a == c && b == d = (a,b) : cross xs ys
-  | a > c = cross x ys
-  | c > a = cross xs y
-  | a == c && b > d = cross x ys
-  | a == c && b < d = cross xs y
-
-
--- part 2
 
 wirelen :: String -> [((Int,Int),Int)]
 wirelen moves = sort $ zip (expand 0 0 $ parse moves) [1..]
 
-crosslen :: [((Int,Int),Int)] -> [((Int,Int),Int)] -> [(Int,Int,Int)]
+crosslen :: [((Int,Int),Int)] -> [((Int,Int),Int)] -> [((Int,Int),Int)]
 crosslen [] _ = []
 crosslen _ [] = []
 crosslen x@(((a,b),p):xs) y@(((c,d),q):ys)
-  | a == c && b == d = (a,b,p+q) : crosslen xs ys
+  | a == c && b == d = ((a,b),p+q) : crosslen xs ys
   | a > c = crosslen x ys
   | c > a = crosslen xs y
   | a == c && b > d = crosslen x ys
   | a == c && b < d = crosslen xs y
 
-run2 :: String -> String -> Int
-run2 a b = minimum $ map (\(x,y,len) -> len) $ crosslen (wirelen a) (wirelen b)
-
 process :: [String] -> [String]
-process (a:b:_) = map show [run a b, run2 a b]
+process (a:b:_) = map (show.minimum) [map dist crosses, map snd crosses]
+    where crosses = crosslen (wirelen a) (wirelen b)
+          dist ((x,y),_) = abs x + abs y
 
 main :: IO ()
 main = interact (unlines . process . lines)
