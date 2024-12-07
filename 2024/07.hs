b41c7f279250c787a67b9d73c065a54e54510c5e
Erik Ekman <erik@kryo.se>
2024-12-07 11:16:01 +0100

    Day 7 much faster

--- 2024/07.hs
+++ 2024/07.hs
@@ -1,12 +1,12 @@
-process as = map (show.sum) (map eval [[sum,product], [sum,product,concat]])
+process as = map (show.sum) (map eval [[(+),(*)], [(+),(*),concat]])
     where eval fns = map (\(t,(a:bs)) -> solve fns t bs a) as
-          concat (a:b:_) = read (show a ++ show b)
+          concat a b = product (a : (take (length $ show b) (repeat 10))) + b
 
-solve :: [([Int]->Int)] -> Int -> [Int] -> Int -> Int
+solve :: [(Int->Int->Int)] -> Int -> [Int] -> Int -> Int
 solve _ tot [] n
     | n == tot = n
     | otherwise = 0
-solve fns tot (a:bs) n = maximum (map (\f -> solve fns tot bs (f [n,a])) fns)
+solve fns tot (a:bs) n = maximum (map (\f -> solve fns tot bs (f n a)) fns)
 
 parse :: String -> (Int, [Int])
 parse s = (read $ init tot, map read parts)
