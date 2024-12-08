651a91c272e7dbbd71cad5cf3be0962b969df4e6
Erik Ekman <erik@kryo.se>
2024-12-08 10:26:08 +0100

    Simpler day 8

--- 2024/08.hs
+++ 2024/08.hs
@@ -1,18 +1,14 @@
 import Data.List
 
-process (rs,cs,as) = map (show.length)[nodes spots, nodes harmonics]
+process (rs,cs,as) = map (show.length.nodes) [[-2,1], [-100..100]]
      where freqs = map (map snd) $ groupBy (\(a,_) (b,_) -> a == b) $ sort as
-           nodes fn = nub $ sort $ filter valid $ concatMap fn freqs
+           nodes rs = nub $ filter valid $ concatMap (harmonics rs) freqs
            valid (r,c) = r >= 0 && r < rs && c >= 0 && c < cs
 
-spots (a:[]) = []
-spots (a:bs) = concatMap (pairs a) bs ++ spots bs
-    where pairs (ar,ac) (br,bc) = [(ar+(ar-br),ac+(ac-bc)), (br-(ar-br),bc-(ac-bc))]
-
-harmonics (a:[]) = []
-harmonics (a:bs) = concatMap (pairs a) bs ++ harmonics bs
+harmonics _ (a:[]) = []
+harmonics rs (a:bs) = concatMap (pairs a) bs ++ harmonics rs bs
     where step (ar,ac) (br,bc) n = (ar+n*(ar-br),ac+n*(ac-bc))
-          pairs a b = map (step a b) [-100..100]
+          pairs a b = map (step a b) rs
 
 parse :: [String] -> (Int, Int, [(Char, (Int,Int))])
 parse ss = (length ss, length (head ss), filter (\(t,p) -> t /= '.') cells)
