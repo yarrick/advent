03dcb69c33523c408a366b7bab45ae78e11fb8c6
Erik Ekman <erik@kryo.se>
2024-12-11 22:04:22 +0100

    Minor cleanup

--- 2024/11.hs
+++ 2024/11.hs
@@ -1,18 +1,18 @@
 import qualified Data.Map as M
 
 process  :: [Int] -> [String]
-process ns = map (show.sum.(map snd).M.toList) [elems !! 25, elems !! 75]
+process ns = map (show.sum.M.elems) [elems !! 25, elems !! 75]
     where op m = foldl step M.empty $ M.toList m
           elems = iterate op $ M.fromList $ zip ns (cycle [1])
 
 step :: M.Map Int Int -> (Int, Int) -> M.Map Int Int
 step mp (a, reps)
-    | a == 0 = M.insertWithKey adder 1 reps mp
-    | even numlen = M.insertWithKey adder m reps $ M.insertWithKey adder n reps mp
-    | otherwise = M.insertWithKey adder (a*2024) reps mp
+    | a == 0 = put 1 mp
+    | even numlen = put m $ put n mp
+    | otherwise =  put (a*2024) mp
     where numlen = length (show a)
           (m,n) = divMod a (product $ take (div numlen 2) $ cycle [10])
-          adder _ old new = old + new
+          put v m = M.insertWithKey (\_ x y -> x + y) v reps m
 
 main :: IO ()
 main = interact (unlines . process . map read . words)
