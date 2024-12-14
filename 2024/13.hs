78b3ad8618b42129489cbf347a64d2df8696c962
Erik Ekman <erik@kryo.se>
2024-12-14 16:19:39 +0100

    Day 13 complete

--- 2024/13.hs
+++ 2024/13.hs
@@ -2,24 +2,15 @@ import Data.Char
 
 type Pos = [Int] -- X, Y
 
-process ts = [show $ 3*sum as + sum bs]
-    where (as,bs) = unzip $ concatMap play ts
+process ts = map (show.run) [ts, map adjust ts]
+    where run ps = sum $ map (\(a,b) -> 3*a+b) $ concatMap play ps
+          adjust (ab,p) = (ab,map (10000000000000+) p)
 
-play ((a, b), goal)
-    | bfirst /= [] = bfirst
-    | otherwise = map (\(a,b) -> (b,a)) $ push ((b,a),goal) 0 (steps a)
-    where steps n = minimum $ map succ $ zipWith div goal n
-          bfirst = push ((a,b),goal) 0 (steps b)
-
-push ((a@(ax:ay:[]), b@(bx:by:[])), goal) as bs
-    | goal == [x,y] = [(as,bs)]
-    | as < 0 || bs < 0 = []
-    | head goal < x && bx > ax = push ((a,b),goal) as (pred bs)
-    | last goal < y && by > ay = push ((a,b),goal) as (pred bs)
-    | x > (head goal + ax + bx) || y > (last goal + ay + by) = []
-    | otherwise = push ((a,b),goal) (succ as) bs
-    where x = as*ax + bs*bx
-          y = as*ay + bs*by
+play ((a@(xa:ya:[]), b@(xb:yb:[])), (px:py:[]))
+    | arest /= 0 || brest /= 0 = []
+    | otherwise = [(a, b)]
+    where (a,arest) = divMod (px*yb - xb*py) (xa*yb - ya*xb)
+          (b,brest) = divMod (py-a*ya) yb
 
 parse :: [String] -> [((Pos, Pos), Pos)]
 parse [] = []
