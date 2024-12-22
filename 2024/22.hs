4c596ead81b7bfe4df70223899a2c389ecee4512
Erik Ekman <erik@kryo.se>
2024-12-22 15:28:37 +0100

    A bit faster day 22

--- 2024/22.hs
+++ 2024/22.hs
@@ -8,17 +8,15 @@ process m = map show [sum $ map last secrets, maximum $ M.elems quads]
           quads = foldr1 (M.unionWith (+)) trades
 
 trends m (a:b:c:[]) = m
-trends m ((a,_):(b,ba):(c,ca):(d,da):es)
-    | M.notMember (a,b,c,d) m = trends (M.insert (a,b,c,d) da m) ((b,ba):(c,ca):(d,da):es)
-    | otherwise = trends m ((b,ba):(c,ca):(d,da):es)
-    where quad = (a,b,c,d)
+trends m ((a,_):(b,ba):(c,ca):(d,da):es) =
+    trends (M.insertWithKey (\k o n -> n) (a,b,c,d) da m) ((b,ba):(c,ca):(d,da):es)
 
 diffs (a:[]) = []
 diffs (a:b:cs) = (diff a b) : diffs (b:cs)
     where diff x y = ((mod y 10) - (mod x 10), mod y 10)
 
-secret n = step (step (step n (*64)) (`div` 32)) (*2048)
-    where step num fn = mod (xor (fn num) num) 16777216
+secret n = step (*2048) $ step (`div` 32) $ step (*64) n
+    where step fn num = mod (xor (fn num) num) 16777216
 
 main :: IO ()
 main = interact (unlines . process . map read . lines)
