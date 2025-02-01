c3b366993032da5f22cb26f1f8f26fc5446164d0
Erik Ekman <erik@kryo.se>
2025-02-01 13:25:19 +0100

    Pin runs to core 0 and 1
    My new laptop has 3 Zen5 and 5 Zen5c cores (and hyperthreading),
    make sure we run a Zen5 core that will clock higher.

--- .run.sh
+++ .run.sh
@@ -7,6 +7,6 @@ do
     file=$(basename $f)
     if [ -e "$f" ]; then
         >&2 echo "Running $BIN with $f as input"
-        time $BIN +RTS -M10g -s.rts_stats_${dir}_${file}.log < $f
+        time taskset --cpu-list 0-1 $BIN +RTS -M10g -s.rts_stats_${dir}_${file}.log < $f
     fi
 done
